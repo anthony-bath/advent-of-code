2fd5c7c4c17db882d849873d5225b2515ce680d6
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2021-12-03 13:45:10 -0500

    Update index.js

--- 03/part-2/index.js
+++ 03/part-2/index.js
@@ -6,37 +6,27 @@ const data = fs
   .split("\n")
   .map((x) => x.trim());
 
-let bitCounts = getBitCounts(data);
-
-let generatorData = [...data];
-let currentBit = 0;
-
-while (generatorData.length > 1) {
-  const mostCommonValue = getMostCommonValue(bitCounts[currentBit]);
-  generatorData = generatorData.filter(
-    (x) => [...x][currentBit] === mostCommonValue
-  );
-  bitCounts = getBitCounts(generatorData);
-  currentBit++;
-}
-
-bitCounts = getBitCounts(data);
-let scrubberData = [...data];
-currentBit = 0;
-
-while (scrubberData.length > 1) {
-  const leastCommonValue = getLeastCommonValue(bitCounts[currentBit]);
-  scrubberData = scrubberData.filter(
-    (x) => [...x][currentBit] === leastCommonValue
-  );
-  bitCounts = getBitCounts(scrubberData);
-  currentBit++;
-}
+const generator = getResultData(data, getMostCommonValue);
+const scrubber = getResultData(data, getLeastCommonValue);
 
 fs.writeFileSync(
   "./03/part-2/output.txt",
-  (parseInt(generatorData[0], 2) * parseInt(scrubberData[0], 2)).toString()
+  (parseInt(generator, 2) * parseInt(scrubber, 2)).toString()
 );
+
+function getResultData(inputData, valueFunc) {
+  let bitCounts = getBitCounts(inputData);
+  let currentBit = 0;
+
+  while (inputData.length > 1) {
+    const targetValue = valueFunc(bitCounts[currentBit]);
+    inputData = inputData.filter((x) => [...x][currentBit] === targetValue);
+    bitCounts = getBitCounts(inputData);
+    currentBit++;
+  }
+
+  return inputData[0];
+}
 
 function getMostCommonValue(bitData) {
   if (bitData[0] > bitData[1]) {
