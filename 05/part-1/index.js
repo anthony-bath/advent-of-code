5841bd0b551bb44adc7b4124e4fbb1d3e8db5520
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2021-12-05 16:57:27 -0500

    Day 5

---
+++ 05/part-1/index.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import {
+  loadData,
+  plotHorizontal,
+  plotVertical,
+  evaluateGrid,
+} from "../util/index.js";
+
+const { lines, grid } = loadData();
+
+for (const line of lines) {
+  const { x1, x2, y1, y2 } = line;
+
+  if (x1 === x2) {
+    plotVertical(y1, y2, x1, grid);
+  } else if (y1 === y2) {
+    plotHorizontal(x1, x2, y1, grid);
+  }
+}
+
+fs.writeFileSync("./05/part-1/output.txt", evaluateGrid(grid));
