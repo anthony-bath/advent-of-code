448a994e27a1b7ca1012ed02aded5550a9a17e4d
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2021-12-07 09:09:55 -0500

    Day 7

---
+++ 07/part-1/index.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+
+const positions = fs
+  .readFileSync("./07/input.txt")
+  .toString()
+  .split(",")
+  .map((n) => parseInt(n));
+
+const maxPos = Math.max(...positions);
+let fuelByPosition = Array(maxPos + 1).fill(0);
+
+for (const position of positions) {
+  fuelByPosition = fuelByPosition.map((fuel, targetPosition) => {
+    return (fuel += Math.abs(position - targetPosition));
+  });
+}
+
+fs.writeFileSync(
+  "./07/part-1/output.txt",
+  Math.min(...fuelByPosition).toString()
+);
