1cb00eba26c8b2738fb5bb0899d9cb8bc917cd07
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2021-12-09 12:51:12 -0500

    Day 9 tidying up

--- 09/part-2/index.js
+++ 09/part-2/index.js
@@ -21,10 +21,7 @@ const visited = [...Array(grid.length + 1)].map((_) =>
 
 for (let i = 0; i < grid.length; i++) {
   for (let j = 0; j < grid[i].length; j++) {
-    const point = grid[i][j];
-    const [u, d, l, r] = getAdjacent(grid, i, j);
-
-    if ([u, d, l, r].every((x) => x > point)) {
+    if (!visited[i][j]) {
       basinSizes.push(search(grid, i, j));
     }
   }
@@ -32,39 +29,34 @@ for (let i = 0; i < grid.length; i++) {
 
 function search(grid, i, j) {
   visited[i][j] = true;
+
+  if (grid[i][j] === 9) {
+    return 0;
+  }
+
   let basinSize = 1;
 
-  const [u, d, l, r] = getAdjacent(grid, i, j);
-
-  if (u < 9 && !visited[i - 1][j]) {
+  if (i > 0 && !visited[i - 1][j]) {
     basinSize += search(grid, i - 1, j);
   }
 
-  if (d < 9 && !visited[i + 1][j]) {
+  if (i < grid.length - 1 && !visited[i + 1][j]) {
     basinSize += search(grid, i + 1, j);
   }
 
-  if (l < 9 && !visited[i][j - 1]) {
+  if (j > 0 && !visited[i][j - 1]) {
     basinSize += search(grid, i, j - 1);
   }
 
-  if (r < 9 && !visited[i][j + 1]) {
+  if (j < grid[i].length - 1 && !visited[i][j + 1]) {
     basinSize += search(grid, i, j + 1);
   }
 
   return basinSize;
 }
 
-function getAdjacent(grid, i, j) {
-  return [
-    i > 0 ? grid[i - 1][j] : 9,
-    i < grid.length - 1 ? grid[i + 1][j] : 9,
-    j > 0 ? grid[i][j - 1] : 9,
-    j < grid[i].length - 1 ? grid[i][j + 1] : 9,
-  ];
-}
-
 basinSizes.sort((x, y) => y - x);
+
 fs.writeFileSync(
   "./09/part-2/output.txt",
   (basinSizes[0] * basinSizes[1] * basinSizes[2]).toString()
