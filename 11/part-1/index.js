f43296e68d58ff47b823ee9eb6258afac18f98bf
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2021-12-11 10:24:50 -0500

    tidy up Day 11 some

--- 11/part-1/index.js
+++ 11/part-1/index.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-const grid = fs
+let grid = fs
   .readFileSync("./11/input.txt")
   .toString()
   .split("\n")
@@ -16,112 +16,51 @@ const STEPS = 100;
 let flashes = 0;
 
 for (let step = 0; step < STEPS; step++) {
-  const flashesThisStep = [...Array(10)].map((_) => Array(10).fill(0));
+  const flashed = [...Array(10)].map((_) => Array(10).fill(0));
   const willFlash = [];
 
-  //increase all energy
-  for (let row = 0; row < 10; row++) {
-    for (let col = 0; col < 10; col++) {
-      grid[row][col] += 1;
+  // Increase Energy of All
+  grid.forEach((row, i) =>
+    row.forEach((_, j) => {
+      grid[i][j] += 1;
 
-      if (grid[row][col] > 9) {
-        willFlash.push([row, col]);
+      if (grid[i][j] > 9) {
+        willFlash.push([i, j]);
       }
-    }
-  }
+    })
+  );
 
   while (willFlash.length > 0) {
     const [row, col] = willFlash.pop();
 
-    if (flashesThisStep[row][col]) {
+    if (flashed[row][col]) {
       continue;
     } else {
-      flashesThisStep[row][col] = 1;
+      flashed[row][col] = 1;
     }
 
-    // increase adjacent energy
-
-    // UP
-    if (row > 0) {
-      grid[row - 1][col] += 1;
-
-      if (grid[row - 1][col] > 9 && !flashesThisStep[row - 1][col]) {
-        willFlash.push([row - 1, col]);
-      }
-    }
-
-    // DOWN
-    if (row < 9) {
-      grid[row + 1][col] += 1;
-
-      if (grid[row + 1][col] > 9 && !flashesThisStep[row + 1][col]) {
-        willFlash.push([row + 1, col]);
-      }
-    }
-
-    // LEFT
-    if (col > 0) {
-      grid[row][col - 1] += 1;
-
-      if (grid[row][col - 1] > 9 && !flashesThisStep[row][col - 1]) {
-        willFlash.push([row, col - 1]);
-      }
-    }
-
-    // RIGHT
-    if (col < 9) {
-      grid[row][col + 1] += 1;
-
-      if (grid[row][col + 1] > 9 && !flashesThisStep[row][col + 1]) {
-        willFlash.push([row, col + 1]);
-      }
-    }
-
-    // UP-LEFT
-    if (row > 0 && col > 0) {
-      grid[row - 1][col - 1] += 1;
-
-      if (grid[row - 1][col - 1] > 9 && !flashesThisStep[row - 1][col - 1]) {
-        willFlash.push([row - 1, col - 1]);
-      }
-    }
-
-    // UP-RIGHT
-    if (row > 0 && col < 9) {
-      grid[row - 1][col + 1] += 1;
-
-      if (grid[row - 1][col + 1] > 9 && !flashesThisStep[row - 1][col + 1]) {
-        willFlash.push([row - 1, col + 1]);
-      }
-    }
-
-    // DOWN-LEFT
-    if (row < 9 && col > 0) {
-      grid[row + 1][col - 1] += 1;
-
-      if (grid[row + 1][col - 1] > 9 && !flashesThisStep[row + 1][col - 1]) {
-        willFlash.push([row + 1, col - 1]);
-      }
-    }
-
-    // DOWN-RIGHT
-    if (row < 9 && col < 9) {
-      grid[row + 1][col + 1] += 1;
-
-      if (grid[row + 1][col + 1] > 9 && !flashesThisStep[row + 1][col + 1]) {
-        willFlash.push([row + 1, col + 1]);
-      }
-    }
+    // Evaluate Neighbours
+    row > 0 && evaluate(willFlash, flashed, row - 1, col);
+    row < 9 && evaluate(willFlash, flashed, row + 1, col);
+    col > 0 && evaluate(willFlash, flashed, row, col - 1);
+    col < 9 && evaluate(willFlash, flashed, row, col + 1);
+    row > 0 && col > 0 && evaluate(willFlash, flashed, row - 1, col - 1);
+    row > 0 && col < 9 && evaluate(willFlash, flashed, row - 1, col + 1);
+    row < 9 && col > 0 && evaluate(willFlash, flashed, row + 1, col - 1);
+    row < 9 && col < 9 && evaluate(willFlash, flashed, row + 1, col + 1);
   }
 
-  // reset flashers
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      if (flashesThisStep[i][j]) {
-        grid[i][j] = 0;
-        flashes += 1;
-      }
-    }
+  // Reset Flashers
+  grid = grid.map((row, i) =>
+    row.map((energy, j) => (flashed[i][j] ? flashes++ && 0 : energy))
+  );
+}
+
+function evaluate(willFlash, flashed, row, col) {
+  grid[row][col] += 1;
+
+  if (grid[row][col] > 9 && !flashed[row][col]) {
+    willFlash.push([row, col]);
   }
 }
 
--- 11/part-2/index.js
+++ 11/part-2/index.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-const grid = fs
+let grid = fs
   .readFileSync("./11/input.txt")
   .toString()
   .split("\n")
@@ -14,117 +14,58 @@ const grid = fs
 let step = 1;
 
 while (true) {
-  const flashesThisStep = [...Array(10)].map((_) => Array(10).fill(0));
+  const flashed = [...Array(10)].map((_) => Array(10).fill(0));
   const willFlash = [];
 
-  //increase all energy
-  for (let row = 0; row < 10; row++) {
-    for (let col = 0; col < 10; col++) {
-      grid[row][col] += 1;
+  // Increase Energy of All
+  grid.forEach((row, i) =>
+    row.forEach((_, j) => {
+      grid[i][j] += 1;
 
-      if (grid[row][col] > 9) {
-        willFlash.push([row, col]);
+      if (grid[i][j] > 9) {
+        willFlash.push([i, j]);
       }
-    }
-  }
+    })
+  );
 
   while (willFlash.length > 0) {
     const [row, col] = willFlash.pop();
 
-    if (flashesThisStep[row][col]) {
+    if (flashed[row][col]) {
       continue;
     } else {
-      flashesThisStep[row][col] = 1;
+      flashed[row][col] = 1;
     }
 
-    // increase adjacent energy
-
-    // UP
-    if (row > 0) {
-      grid[row - 1][col] += 1;
-
-      if (grid[row - 1][col] > 9 && !flashesThisStep[row - 1][col]) {
-        willFlash.push([row - 1, col]);
-      }
-    }
-
-    // DOWN
-    if (row < 9) {
-      grid[row + 1][col] += 1;
-
-      if (grid[row + 1][col] > 9 && !flashesThisStep[row + 1][col]) {
-        willFlash.push([row + 1, col]);
-      }
-    }
-
-    // LEFT
-    if (col > 0) {
-      grid[row][col - 1] += 1;
-
-      if (grid[row][col - 1] > 9 && !flashesThisStep[row][col - 1]) {
-        willFlash.push([row, col - 1]);
-      }
-    }
-
-    // RIGHT
-    if (col < 9) {
-      grid[row][col + 1] += 1;
-
-      if (grid[row][col + 1] > 9 && !flashesThisStep[row][col + 1]) {
-        willFlash.push([row, col + 1]);
-      }
-    }
-
-    // UP-LEFT
-    if (row > 0 && col > 0) {
-      grid[row - 1][col - 1] += 1;
-
-      if (grid[row - 1][col - 1] > 9 && !flashesThisStep[row - 1][col - 1]) {
-        willFlash.push([row - 1, col - 1]);
-      }
-    }
-
-    // UP-RIGHT
-    if (row > 0 && col < 9) {
-      grid[row - 1][col + 1] += 1;
-
-      if (grid[row - 1][col + 1] > 9 && !flashesThisStep[row - 1][col + 1]) {
-        willFlash.push([row - 1, col + 1]);
-      }
-    }
-
-    // DOWN-LEFT
-    if (row < 9 && col > 0) {
-      grid[row + 1][col - 1] += 1;
-
-      if (grid[row + 1][col - 1] > 9 && !flashesThisStep[row + 1][col - 1]) {
-        willFlash.push([row + 1, col - 1]);
-      }
-    }
-
-    // DOWN-RIGHT
-    if (row < 9 && col < 9) {
-      grid[row + 1][col + 1] += 1;
-
-      if (grid[row + 1][col + 1] > 9 && !flashesThisStep[row + 1][col + 1]) {
-        willFlash.push([row + 1, col + 1]);
-      }
-    }
+    // Evaluate Neighbours
+    row > 0 && evaluate(willFlash, flashed, row - 1, col);
+    row < 9 && evaluate(willFlash, flashed, row + 1, col);
+    col > 0 && evaluate(willFlash, flashed, row, col - 1);
+    col < 9 && evaluate(willFlash, flashed, row, col + 1);
+    row > 0 && col > 0 && evaluate(willFlash, flashed, row - 1, col - 1);
+    row > 0 && col < 9 && evaluate(willFlash, flashed, row - 1, col + 1);
+    row < 9 && col > 0 && evaluate(willFlash, flashed, row + 1, col - 1);
+    row < 9 && col < 9 && evaluate(willFlash, flashed, row + 1, col + 1);
   }
 
-  // reset flashers
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      if (flashesThisStep[i][j]) {
-        grid[i][j] = 0;
-      }
-    }
-  }
+  // Reset Flashers
+  let flashes = 0;
+  grid = grid.map((row, i) =>
+    row.map((energy, j) => (flashed[i][j] ? flashes++ && 0 : energy))
+  );
 
-  if (flashesThisStep.every((row) => row.every((octopus) => octopus === 1))) {
+  if (flashes === 100) {
     break;
   } else {
     step++;
+  }
+}
+
+function evaluate(willFlash, flashed, row, col) {
+  grid[row][col] += 1;
+
+  if (grid[row][col] > 9 && !flashed[row][col]) {
+    willFlash.push([row, col]);
   }
 }
 
