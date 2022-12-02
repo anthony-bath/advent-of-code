2fad11e4d5291845371a79284227850642007a41
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2022-12-02 08:50:34 -0500

    Day 2 solutions

---
+++ 02/part-1.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+
+// Rock (1) = A, X
+// Paper (2) = B, Y
+// Scissors (3) = C, Z
+
+const scoreByPlays = new Map([
+  ['A X', 1 + 3],
+  ['A Y', 2 + 6],
+  ['A Z', 3 + 0],
+  ['B X', 1 + 0],
+  ['B Y', 2 + 3],
+  ['B Z', 3 + 6],
+  ['C X', 1 + 6],
+  ['C Y', 2 + 0],
+  ['C Z', 3 + 3],
+]);
+
+const strategyScore = fs
+  .readFileSync('./02/input.txt')
+  .toString()
+  .split('\n')
+  .reduce((score, plays) => (score += scoreByPlays.get(plays)), 0);
+
+fs.writeFileSync('./02/output-1.txt', `${strategyScore}`);
