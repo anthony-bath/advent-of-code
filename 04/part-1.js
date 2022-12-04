b8def162868bc1c4e5d9726e1945c2d5b8f53280
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2022-12-04 08:15:55 -0500

    Day 4 Solution

---
+++ 04/part-1.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+
+const overlappingPairCount = fs
+  .readFileSync('./04/input.txt')
+  .toString()
+  .split('\n')
+  .reduce((count, ranges) => {
+    const [r1, r2] = ranges.split(',');
+    const [r1Start, r1End] = r1.split('-').map((n) => parseInt(n));
+    const [r2Start, r2End] = r2.split('-').map((n) => parseInt(n));
+
+    if ((r2Start >= r1Start && r2End <= r1End) || (r1Start >= r2Start && r1End <= r2End)) {
+      return ++count;
+    }
+
+    return count;
+  }, 0);
+
+fs.writeFileSync('./04/output-1.txt', `${overlappingPairCount}`);
