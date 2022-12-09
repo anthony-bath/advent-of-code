27b72e46b2afabeeaa5d4a741f79a7b57a2daf95
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2022-12-08 19:57:54 -0500

    Fix Comment

--- 08/part-1.js
+++ 08/part-1.js
@@ -43,7 +43,7 @@ for (let row = 0; row < grid.length; row++) {
         continue;
       }
 
-      // Check South
+      // Check south
       if (evaluate('col', row + 1, col, treeHeight, grid.length)) {
         visibleTreeCount++;
         continue;
