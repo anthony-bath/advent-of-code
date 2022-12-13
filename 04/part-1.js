c97ad707e8c349f7209520d36ece50608176cb25
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2022-12-12 20:55:28 -0500

    Day 4 Solution

---
+++ 04/part-1.js
@@ -0,0 +1,18 @@
+import { read, write } from '../utility.js';
+
+let validCount = 0;
+let fields = [];
+
+read(4).forEach((line) => {
+  if (!line) {
+    validCount +=
+      fields.length === 8 || (fields.length === 7 && fields.indexOf('cid') === -1) ? 1 : 0;
+
+    fields = [];
+    return;
+  }
+
+  fields.push(...line.split(' ').map((pairs) => pairs.split(':')[0]));
+});
+
+write(4, 1, `${validCount}`);
