a29b8cc03132b896a84f5f7cdf8fbf121a56af02
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2022-12-14 17:02:29 -0500

    Day 14 Part 2 Optimization First Pass

--- 14/part-2.js
+++ 14/part-2.js
@@ -2,8 +2,9 @@ import { write, read } from '../utility.js';
 
 const HEIGHT = 183;
 
-const rocks = new Set();
+const blockers = new Set();
 
+console.time('Day 14 Part 2');
 read(14).forEach((line) => {
   const points = line.split(' -> ').map((coords) => {
     const [x, y] = coords.split(',').map((n) => parseInt(n, 10));
@@ -17,29 +18,34 @@ read(14).forEach((line) => {
     if (p1.x === p2.x) {
       // vertical line
       for (let j = Math.min(p1.y, p2.y); j <= Math.max(p1.y, p2.y); j++) {
-        rocks.add(`${p1.x},${j}`);
+        blockers.add(`${p1.x},${j}`);
       }
     } else {
       // horizontal line
       for (let j = Math.min(p1.x, p2.x); j <= Math.max(p1.x, p2.x); j++) {
-        rocks.add(`${j},${p1.y}`);
+        blockers.add(`${j},${p1.y}`);
       }
     }
   }
 });
 
 let grain = { x: 500, y: 0, atRest: false };
-const grains = new Set();
+//const grains = new Set();
+const path = [];
+let grainCount = 0;
 
 while (true) {
   if (grain.atRest) {
-    grains.add(`${grain.x},${grain.y}`);
+    //grains.add(`${grain.x},${grain.y}`);
+    blockers.add(`${grain.x},${grain.y}`);
+    grainCount++;
 
     if (grain.x === 500 && grain.y === 0) {
       break;
     }
 
-    grain = { x: 500, y: 0, atRest: false };
+    const { x, y } = path.pop();
+    grain = { x, y, atRest: false };
     continue;
   }
 
@@ -51,11 +57,14 @@ while (true) {
   }
 
   const { xDelta, yDelta } = moveDelta;
+  path.push({ x: grain.x, y: grain.y });
   grain.x += xDelta;
   grain.y += yDelta;
 }
 
-write(14, 2, `${grains.size}`);
+console.timeEnd('Day 14 Part 2');
+
+write(14, 2, `${grainCount}`);
 
 function getMoveDelta({ x, y }) {
   if (y + 1 >= HEIGHT) {
@@ -66,11 +75,11 @@ function getMoveDelta({ x, y }) {
   const downLeft = `${x - 1},${y + 1}`;
   const downRight = `${x + 1},${y + 1}`;
 
-  if (!rocks.has(down) && !grains.has(down)) {
+  if (!blockers.has(down)) {
     return { xDelta: 0, yDelta: 1 };
-  } else if (!rocks.has(downLeft) && !grains.has(downLeft)) {
+  } else if (!blockers.has(downLeft)) {
     return { xDelta: -1, yDelta: 1 };
-  } else if (!rocks.has(downRight) && !grains.has(downRight)) {
+  } else if (!blockers.has(downRight)) {
     return { xDelta: 1, yDelta: 1 };
   }
 }
