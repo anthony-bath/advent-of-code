a179b275277bb53867f65b3d471b0f7f8d430d4d
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2022-12-13 20:22:05 -0500

    Day 13 Complete

---
+++ 13/part-2.js
@@ -0,0 +1,53 @@
+import { read, write } from '../utility.js';
+
+function compare(left, right) {
+  if (Number.isInteger(left) && Number.isInteger(right)) {
+    if (left < right) {
+      return -1;
+    } else if (left === right) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+
+  if (Array.isArray(left) && Number.isInteger(right)) {
+    return compare(left, [right]);
+  }
+
+  if (Number.isInteger(left) && Array.isArray(right)) {
+    return compare([left], right);
+  }
+
+  // both Arrays
+  for (let i = 0; i < Math.min(left.length, right.length); i++) {
+    const result = compare(left[i], right[i]);
+
+    if (result === -1) {
+      return -1;
+    } else if (result === 1) {
+      return 1;
+    }
+  }
+
+  // if we got here, the comparisons have been all equality
+  if (left.length < right.length) {
+    return -1;
+  } else if (right.length < left.length) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+const input = read(13);
+const packets = [[[2]], [[6]]];
+
+for (const line of input) {
+  if (!line) continue;
+  packets.push(JSON.parse(line));
+}
+
+const stringified = packets.sort(compare).map((packet) => JSON.stringify(packet));
+
+write(13, 2, `${(1 + stringified.indexOf('[[6]]')) * (1 + stringified.indexOf('[[2]]'))}`);
