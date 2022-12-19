33d01608bb868869c75dfb09e7c5f9bc890956db
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2022-12-18 20:29:33 -0500

    Day 6 Complete

---
+++ 06/part-1.js
@@ -0,0 +1,23 @@
+import { read, write } from '../utility.js';
+
+const input = read(6);
+
+let total = 0;
+let currentGroup = new Set();
+
+for (let i = 0; i < input.length; i++) {
+  const line = input[i];
+
+  if (!line) {
+    total += currentGroup.size;
+    currentGroup = new Set();
+  } else {
+    line.split('').forEach((question) => currentGroup.add(question));
+
+    if (i === input.length - 1) {
+      total += currentGroup.size;
+    }
+  }
+}
+
+write(6, 1, `${total}`);
