29ef4294126d69639c82fdc1cad2db3625eea888
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2022-12-26 19:43:34 -0500

    Day 8 Complete

---
+++ 08/part-1.js
@@ -0,0 +1,41 @@
+import { read, write } from '../utility.js';
+
+const instructions = read(8).map((instruction) => {
+  const [operation, value] = instruction.split(' ');
+  return [operation, operation === 'nop' ? 0 : Number(value)];
+});
+
+function getJumpInstruction(index, value) {
+  if (index === 0) {
+    return index - 1;
+  }
+
+  if (index > 0) {
+    return index + value - 1;
+  }
+
+  return index - (Math.abs(value) + 1);
+}
+
+let accumulator = 0;
+let executed = new Set();
+
+for (let i = 0; i < instructions.length; i++) {
+  if (executed.has(i)) break;
+
+  const [operation, value] = instructions[i];
+  executed.add(i);
+
+  if (operation === 'nop') continue;
+
+  switch (operation) {
+    case 'acc':
+      accumulator += value;
+      break;
+
+    case 'jmp':
+      i = getJumpInstruction(i, value);
+  }
+}
+
+write(8, 1, `${accumulator}`);
