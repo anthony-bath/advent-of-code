3663343da2e665dd5642e73b6bfacd6ff876c665
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2022-12-27 09:11:48 -0500

    Day 9 Complete

---
+++ 09/part-1.js
@@ -0,0 +1,41 @@
+import { read, write } from '../utility.js';
+
+const SIZE = 25;
+
+const input = read(9).map((n) => Number(n));
+const preamble = input.slice(0, SIZE);
+let sums = [];
+const used = new Set();
+
+for (let i = 0; i < SIZE; i++) {
+  preamble.forEach((value, j) => {
+    const key1 = `${i}-${j}`;
+    const key2 = `${j}-${i}`;
+
+    if (j !== i && !used.has(key1) && !used.has(key2)) {
+      sums.push({ indexes: [i, j], value: input[i] + value });
+      used.add(key1);
+      used.add(key2);
+    }
+  });
+}
+
+let result;
+
+for (let i = SIZE; i < input.length; i++) {
+  if (sums.filter((sum) => sum.value === input[i]).length === 0) {
+    result = input[i];
+    break;
+  }
+
+  const start = i - (SIZE - 1);
+  const prev = start - 1;
+
+  sums = sums.filter(({ indexes }) => !indexes.includes(prev));
+
+  input
+    .slice(start, start + SIZE)
+    .forEach((value, j) => sums.push({ indexes: [i, j + start], value: input[i] + value }));
+}
+
+write(9, 1, `${result}`);
