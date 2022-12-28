f4dfce970b10033c5ea6d6d19cc10b103f5e4264
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2022-12-28 09:41:34 -0500

    Day 14 Complete

--- utility.js
+++ utility.js
@@ -6,6 +6,8 @@ export function write(day, part, content) {
   fs.writeFileSync(`./${day.toString().padStart(2, '0')}/output-${part}.txt`, content);
 }
 
-export function read(day, splitBy = EOL) {
-  return fs.readFileSync(`./${day.toString().padStart(2, '0')}/input.txt`, 'utf-8').split(splitBy);
+export function read(day, { test = false, splitBy = EOL }) {
+  return fs
+    .readFileSync(`./${day.toString().padStart(2, '0')}/${test ? 'test' : ''}input.txt`, 'utf-8')
+    .split(splitBy);
 }
