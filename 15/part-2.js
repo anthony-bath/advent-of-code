c0c54d8232c7a43c29a2a045593baf4b70a7b1cf
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2022-12-28 14:52:03 -0500

    Day 15 Complete

---
+++ 15/part-2.js
@@ -0,0 +1,34 @@
+import { read, write } from '../utility.js';
+
+const numbers = read(15, { splitBy: ',' }).map((n) => Number(n));
+const TURNS = 30000000;
+
+let turn = 1;
+const spoken = new Map();
+let lastSpoken = null;
+const cache = {};
+
+while (turn <= TURNS) {
+  if (turn <= numbers.length) {
+    lastSpoken = numbers[turn - 1];
+    spoken.set(lastSpoken, [turn]);
+  } else {
+    const spokenOnTurns = spoken.get(lastSpoken);
+
+    if (spokenOnTurns.length === 1) {
+      lastSpoken = 0;
+    } else {
+      lastSpoken = spokenOnTurns[1] - spokenOnTurns[0];
+    }
+
+    if (spoken.has(lastSpoken)) {
+      spoken.set(lastSpoken, [spoken.get(lastSpoken).pop(), turn]);
+    } else {
+      spoken.set(lastSpoken, [turn]);
+    }
+  }
+
+  turn++;
+}
+
+write(15, 2, lastSpoken);
