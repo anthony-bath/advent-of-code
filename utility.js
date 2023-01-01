7e8b92123a24fecc65b76d931bab7fa4a93d8204
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2022-12-31 23:11:12 -0500

    add timing to read and write utility functions

--- utility.js
+++ utility.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { EOL } from 'os';
 
 export function write(year, day, part, content) {
+  console.timeEnd(getTimingLabel(year, day));
   console.log(content);
 
   if (!(typeof content === 'string')) {
@@ -19,9 +20,15 @@ export function read(year, day, { test = false, splitBy = EOL, part = null } = {
     'utf-8'
   );
 
+  console.time(getTimingLabel(year, day));
+
   return splitBy ? data.split(splitBy) : data;
 }
 
 export function output(content) {
   fs.writeFileSync('./debug.txt', content);
+}
+
+function getTimingLabel(year, day) {
+  return `${year}-${day.toString().padStart(2, '0')} Time`;
 }
