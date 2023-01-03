77c186758f6c4870f4d2b91fb4802638a61b479d
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-01-02 20:16:40 -0500

    2020 Day 19 Complete

--- 2020/19/part-2.js
+++ 2020/19/part-2.js
@@ -84,41 +84,74 @@ function buildRule(number) {
 }
 
 const baseRule0 = buildRule('0');
-// const rule11 = cache['ll'];
-// const rule8 = cache['8'];
 const rule42 = cache['42'];
 const rule31 = cache['31'];
 
 const matched = [];
-const unmatched = [];
-
-// Need to go 8 11
-// 8 can be 42 repeating
-// 11 can be 42 31 repeating, ending in a 31
-// e.g. 42 (8) 42 (11) 31 (11) 31 (11)
-// minimum length is therefore 8x4 = 32
+const chunkExpression = new RegExp(`.{${rule42[0].length}}`, 'g');
 
 messages.forEach((message) => {
   if (baseRule0.includes(message)) {
     matched.push(message);
   } else if (
+    message.length >= 32 &&
     rule42.includes(message.substring(0, 8)) &&
     rule31.includes(message.substring(message.length - 8))
   ) {
-    unmatched.push(message);
-  }
-});
+    const chunks = message.match(chunkExpression);
 
-unmatched.forEach((message) => {
-  const parts = message.match(/.{8}/g);
+    let count31 = 0;
+    let count42 = 0;
+    const structure = [];
 
-  // already know the first part matches rule 8 aka rule 42 and last part matches rule 11 end aka rule 31
-  parts.shift();
-  parts.pop();
+    for (const chunk of chunks) {
+      if (rule42.includes(chunk)) {
+        structure.push(42);
+        count42++;
+      } else if (rule31.includes(chunk)) {
+        structure.push(31);
+        count31++;
+      }
+    }
 
-  if (parts.every((part) => rule42.includes(part))) {
-    matched.push(message);
-    return;
+    if (structure[1] === 31) {
+      // can never have 31 in second position because there
+      // wouldn't be anough 42s to meet the other criteria
+      return;
+    }
+
+    if (count31 >= count42) {
+      // can never have more 31s than 42s; Rule 11 will always
+      // have the same number of 42s and 31s (see below
+      // Rule 8 will then have at least 1 additional 42
+      return;
+    }
+
+    let changes = 0;
+    for (let i = 1; i < structure.length; i++) {
+      if (structure[i] !== structure[i - 1]) changes++;
+    }
+
+    if (changes === 1) {
+      // if there is only a single change from 42 to 31, it's a valid message
+      // i.e. going from 42 to 31 and then back to 42 again would represent
+      //      an invalid structure
+      //
+      //For example, consider the (invalid) structure:
+      //   42, 42, 31, 42, 31
+      //    A   B   C   D   E
+      // A is as far as Rule 8 matching can go because if it went to B, Rule 11
+      // would not match as it starts with 42 and C is 31. Therefore, Rule 11
+      // matching starts at B. But to "extend" the Rule 11 further, there needs
+      // to be more consecutive 42s, because if you consider the loop portion
+      // of rule 11 (42 11 31) that could become 42 42 11 31 31, and then
+      // 42 42 42 11 31 31 31, and then 42 42 42 42 11 31 31 31 31 and so on,
+      // and it could then terminate with the non-looping Rule 11 (42 31) i.e.
+      // 42 42 42 42 42 31 31 31 31 31. So then after some number of 42s from
+      // Rule 8, the overall structure represented like this could only ever
+      // go from 42 -> 31 once and never back to 42 again
+      matched.push(message);
+    }
   }
 });
 
