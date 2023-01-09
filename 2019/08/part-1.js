533dab3023eb49d1df96c7acf52807831fb0cf9a
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-01-09 18:16:15 -0500

    2019 Day 8 Part 1 Complete

---
+++ 2019/08/part-1.js
@@ -0,0 +1,24 @@
+import { read, write } from '../../utility.js';
+
+const [YEAR, DAY, PART] = [2019, 8, 1];
+
+const WIDTH = 25;
+const HEIGHT = 6;
+
+const layers = {};
+
+read(YEAR, DAY, { splitBy: '' }).forEach((pixel, i) => {
+  const layer = Math.floor(i / (WIDTH * HEIGHT));
+
+  if (!layers[layer]) {
+    layers[layer] = [0, 0, 0];
+  }
+
+  layers[layer][Number(pixel)]++;
+});
+
+const [_, ones, twos] = Object.values(layers)
+  .sort((a, b) => a[0] - b[0])
+  .shift();
+
+write(YEAR, DAY, PART, ones * twos);
