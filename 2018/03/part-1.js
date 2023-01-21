47b3894903ea3fe66ee9a7feaf0d95a3bf991223
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-01-21 18:56:08 -0500

    2018 Day 3 Part 1 Complete Part 2 In Progress

---
+++ 2018/03/part-1.js
@@ -0,0 +1,26 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2018, 3, 1];
+
+const fabric = [...Array(1000).keys()].map((_) => Array(1000).fill('.'));
+
+read(YEAR, DAY, PART).forEach((line) => {
+  const [_, x, y, w, h] = line.match(/\d+/g).map((n) => Number(n));
+
+  for (let row = y; row < y + h; row++) {
+    for (let col = x; col < x + w; col++) {
+      if (fabric[row][col] === '.') {
+        fabric[row][col] = '#';
+      } else {
+        fabric[row][col] = 'X';
+      }
+    }
+  }
+});
+
+write(
+  YEAR,
+  DAY,
+  PART,
+  fabric.reduce((count, row) => count + row.filter((square) => square === 'X').length, 0)
+);
