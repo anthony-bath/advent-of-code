fe6946af274d59440be8c9d6795abe357032fdcf
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-01-31 19:34:14 -0500

    2018 Day 14 Part 2 Minor Improvement

--- 2018/14/part-2.js
+++ 2018/14/part-2.js
@@ -4,8 +4,12 @@ import { Recipe, advance } from './common.js';
 const [YEAR, DAY, PART] = [2018, 14, 2];
 
 const scoreSequence = read(YEAR, DAY, PART)[0];
-const recipes = [new Recipe(3), new Recipe(7)];
 
+const start = Number(scoreSequence.at(0));
+const end = Number(scoreSequence.slice(-1));
+const length = scoreSequence.length;
+
+const recipes = [new Recipe(3), new Recipe(7)];
 let one = recipes[0];
 let two = recipes[1];
 
@@ -15,9 +19,6 @@ two.setNext(one).setPrev(one);
 const head = one;
 let tail = two;
 let count = 2;
-const start = Number(scoreSequence.at(0));
-const end = Number(scoreSequence.slice(-1));
-const length = scoreSequence.length;
 let lastStartCount = 0;
 
 while (true) {
@@ -38,15 +39,19 @@ while (true) {
     }
 
     if (count - lastStartCount === length - 1 && tail.value === end) {
-      const output = [];
-      let pointer = tail;
+      let pointer = tail.prev;
+      let index = length - 2;
 
-      while (output.length < scoreSequence.length) {
-        output.unshift(pointer.value);
-        pointer = pointer.prev;
+      while (true) {
+        if (pointer.value == scoreSequence.at(index)) {
+          pointer = pointer.prev;
+          index--;
+        } else {
+          break;
+        }
       }
 
-      if (output.join('') === scoreSequence) {
+      if (index === -1) {
         done = true;
         break;
       }
