dbc9031e3ce7ac50eee4f7588d97c7a264d93655
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-02-05 13:45:41 -0500

    2017 Day 3 Part 1 Complete

---
+++ 2017/03/part-1.js
@@ -0,0 +1,35 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2017, 3, 1];
+
+const location = Number(read(YEAR, DAY, PART));
+
+let sideLength = Math.ceil(Math.sqrt(location));
+
+if (sideLength % 2 === 0) {
+  sideLength++;
+}
+
+const bottomCenter = sideLength * sideLength - Math.floor(sideLength / 2);
+const centers = [...Array(4).keys()].map((i) => bottomCenter - i * (sideLength - 1));
+const centerDiffs = centers.map((center) => Math.abs(location - center));
+const distance = Math.floor(sideLength / 2);
+
+write(YEAR, DAY, PART, distance + Math.min(...centerDiffs));
+
+//1   => 2-9      (3)
+//2   => 10-25    (5)
+//3   => 26-49    (7)
+//4   => 50-81    (9)
+//5   => 82-121   (11)
+//6   => 122-169  (13)
+//7   => 170-225  (15)
+//8   => 226-289  (17)
+//9   => 290-361  (19)
+//10  => 326-441  (21)
+//11  => 442-529  (23)
+//12  => 530-625  (25)
+//13  => 626-729  (27)
+//14  => 730-841  (29)
+//15  => 842-961  (31)
+//16  => 952-1089 (33)
