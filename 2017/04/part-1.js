bb7a0d599fcf91a9791633b03abb477fa0a35cc3
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-02-06 12:14:30 -0500

    2017 Day 4 Complete

---
+++ 2017/04/part-1.js
@@ -0,0 +1,27 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2017, 4, 1];
+
+let validCount = 0;
+
+read(YEAR, DAY, PART).forEach((passphrase) => {
+  const words = passphrase.split(' ');
+
+  const lookup = {};
+  let isValid = true;
+
+  for (const word of words) {
+    if (!lookup[word]) {
+      lookup[word] = 1;
+    } else {
+      isValid = false;
+      break;
+    }
+  }
+
+  if (isValid) {
+    validCount++;
+  }
+});
+
+write(YEAR, DAY, PART, validCount);
---
+++ 2017/04/part-2.js
@@ -0,0 +1,40 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2017, 4, 2];
+
+function isAnagram(word1, word2) {
+  if (word1.length !== word2.length) {
+    return false;
+  }
+
+  return [...word1].every((letter) => word2.includes(letter));
+}
+
+let validCount = 0;
+
+read(YEAR, DAY, PART).forEach((passphrase) => {
+  const words = passphrase.split(' ');
+
+  let isValid = true;
+
+  for (const [i, word1] of words.entries()) {
+    for (const [j, word2] of words.entries()) {
+      if (i === j) continue;
+
+      if (isAnagram(word1, word2)) {
+        isValid = false;
+        break;
+      }
+    }
+
+    if (!isValid) {
+      break;
+    }
+  }
+
+  if (isValid) {
+    validCount++;
+  }
+});
+
+write(YEAR, DAY, PART, validCount);
