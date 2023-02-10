fb38556c6c907d470ac5d2024bec2d6ca767bcb3
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-02-10 12:46:15 -0500

    2017 Day 8 Complete

---
+++ 2017/08/common.js
@@ -0,0 +1,10 @@
+export const ops = {
+  '<': (a, b) => a < b,
+  '>': (a, b) => a > b,
+  '>=': (a, b) => a >= b,
+  '<=': (a, b) => a <= b,
+  '==': (a, b) => a === b,
+  '!=': (a, b) => a !== b,
+  dec: (a, b) => a - b,
+  inc: (a, b) => a + b,
+};
