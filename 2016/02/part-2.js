e411dd71198933afd4f6e1a82825325370b50626
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-02-15 12:25:35 -0500

    2016 Day 2 Complete

---
+++ 2016/02/part-2.js
@@ -0,0 +1,39 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2016, 2, 2];
+
+const inputs = read(YEAR, DAY, PART).map((line) => line.split(''));
+
+const keypad = [
+  [null, null, 1, null, null],
+  [null, 2, 3, 4, null],
+  [5, 6, 7, 8, 9],
+  [null, 'A', 'B', 'C', null],
+  [null, null, 'D', null, null],
+];
+
+let x = 0;
+let y = 2;
+
+const code = inputs.reduce((code, line) => {
+  for (const input of line) {
+    switch (input) {
+      case 'L':
+        if (x - 1 >= 0 && keypad[y][x - 1] !== null) x--;
+        break;
+      case 'R':
+        if (x + 1 <= 4 && keypad[y][x + 1] !== null) x++;
+        break;
+      case 'U':
+        if (y - 1 >= 0 && keypad[y - 1][x] !== null) y--;
+        break;
+      case 'D':
+        if (y + 1 <= 4 && keypad[y + 1][x] !== null) y++;
+        break;
+    }
+  }
+
+  return [...code, keypad[y][x]];
+}, []);
+
+write(YEAR, DAY, PART, code.join(''));
