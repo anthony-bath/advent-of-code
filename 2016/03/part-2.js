6c58185f5999086fdd6a1d3fa4edc7665911f2a8
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-02-17 08:12:19 -0500

    2016 Day 3 Complete

---
+++ 2016/03/part-2.js
@@ -0,0 +1,36 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2016, 3, 2];
+
+const values = read(YEAR, DAY, PART).map((line) => {
+  return line
+    .trim()
+    .split(/\s+/)
+    .map((n) => Number(n));
+});
+
+function isTriangle(a, b, c) {
+  return a + b > c && a + c > b && b + c > a;
+}
+
+let possibleCount = 0;
+
+for (let row = 0; row < values.length; row += 3) {
+  const [a1, a2, a3] = values[row];
+  const [b1, b2, b3] = values[row + 1];
+  const [c1, c2, c3] = values[row + 2];
+
+  if (isTriangle(a1, b1, c1)) {
+    possibleCount++;
+  }
+
+  if (isTriangle(a2, b2, c2)) {
+    possibleCount++;
+  }
+
+  if (isTriangle(a3, b3, c3)) {
+    possibleCount++;
+  }
+}
+
+write(YEAR, DAY, PART, possibleCount);
