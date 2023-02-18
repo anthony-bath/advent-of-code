28ccde3e1b1bf23a0c1751b3449c83c871881505
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-02-18 07:49:35 -0500

    2016 Day 4 Part 1 Complete

---
+++ 2016/04/part-1.js
@@ -0,0 +1,46 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2016, 4, 1];
+
+const sectorExpression = new RegExp('\\d+', 'g');
+const checksumExpression = new RegExp('[a-z]+', 'g');
+
+function checksumSort([l1, c1], [l2, c2]) {
+  if (c1 !== c2) {
+    return c2 - c1;
+  }
+
+  return l1 < l2 ? -1 : 1;
+}
+
+const result = read(YEAR, DAY, PART).reduce((sum, line) => {
+  const parts = line.split('-');
+  const last = parts.pop();
+  const sector = Number(last.match(sectorExpression)[0]);
+  const labelledChecksum = last.match(checksumExpression)[0];
+
+  const data = {};
+
+  for (const part of parts) {
+    for (const char of part) {
+      if (!(char in data)) {
+        data[char] = 0;
+      }
+
+      data[char]++;
+    }
+  }
+
+  const actualChecksum = Object.entries(data)
+    .sort(checksumSort)
+    .map(([l]) => l)
+    .join('');
+
+  if (actualChecksum.startsWith(labelledChecksum)) {
+    return sum + sector;
+  }
+
+  return sum;
+}, 0);
+
+write(YEAR, DAY, PART, result);
