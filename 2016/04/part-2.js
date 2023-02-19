3c8a0aaf00b5ee4182897e01fed3d6b13849fc9b
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-02-19 07:49:18 -0500

    2016 Day 4 Complete

---
+++ 2016/04/part-2.js
@@ -0,0 +1,37 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2016, 4, 2];
+
+const sectorExpression = new RegExp('\\d+', 'g');
+
+let result = null;
+
+read(YEAR, DAY, PART).forEach((line) => {
+  if (result) {
+    return;
+  }
+
+  const parts = line.split('-');
+  const last = parts.pop();
+  const sector = Number(last.match(sectorExpression)[0]);
+  const shifts = sector % 26;
+
+  const output = [];
+
+  for (const part of parts) {
+    const word = [];
+
+    for (const char of part) {
+      const code = char.charCodeAt(0) - 97;
+      word.push(String.fromCharCode(((code + shifts) % 26) + 97));
+    }
+
+    output.push(word.join(''));
+  }
+
+  if (output.includes('northpole')) {
+    result = sector;
+  }
+});
+
+write(YEAR, DAY, PART, result);
