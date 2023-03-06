7f01f2348979fe24505182bbc120312ee0e1da97
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-06 12:35:58 -0500

    2016 Day 14 Complete

--- 2016/14/part-2.js
+++ 2016/14/part-2.js
@@ -1,7 +1,7 @@
 import { read, write } from '../../utilities/io.js';
 import md5 from 'md5';
 
-const [YEAR, DAY, PART] = [2016, 14, 1];
+const [YEAR, DAY, PART] = [2016, 14, 2];
 
 const salt = read(YEAR, DAY, PART, { splitBy: null });
 
@@ -14,7 +14,7 @@ function checkNext1000(fromIndex, character) {
 
   for (let checkIndex = fromIndex; checkIndex < fromIndex + 1000; checkIndex++) {
     if (!(checkIndex in hashes)) {
-      hashes[checkIndex] = md5(`${salt}${checkIndex}`);
+      hashes[checkIndex] = stretchHash(`${salt}${checkIndex}`);
     }
 
     if (expr.test(hashes[checkIndex])) {
@@ -25,11 +25,21 @@ function checkNext1000(fromIndex, character) {
   return false;
 }
 
+function stretchHash(value) {
+  let result = md5(value);
+
+  for (let i = 0; i < 2016; i++) {
+    result = md5(result);
+  }
+
+  return result;
+}
+
 const expr = /([a-zA-Z0-9])\1\1/;
 
 while (keys.length < 64) {
   if (!(index in hashes)) {
-    hashes[index] = md5(`${salt}${index}`);
+    hashes[index] = stretchHash(`${salt}${index}`);
   }
 
   const result = expr.exec(hashes[index]);
