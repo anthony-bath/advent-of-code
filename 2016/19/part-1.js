d53689e9afe15dd67764fd132cda2d3f47915748
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-08 07:31:42 -0500

    2016 Day 19 Part 1 Complete

---
+++ 2016/19/part-1.js
@@ -0,0 +1,10 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2016, 19, 1];
+
+const n = Number(read(YEAR, DAY, PART, { splitBy: null }));
+
+// Josephus Problem
+const result = 2 * (n - Math.pow(2, Math.floor(Math.log2(n)))) + 1;
+
+write(YEAR, DAY, PART, result);
