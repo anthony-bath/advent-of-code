412e987ff060b695f76622c779a6fd2f841cc45d
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-11 21:49:35 -0500

    2016 Day 11 Input Specific Optimizations

--- 2016/11/part-1.js
+++ 2016/11/part-1.js
@@ -57,8 +57,23 @@ function updateQueueIfSafe(nextData, currentFloor, nextFloor, nextSteps, queue, 
     if (!visited[nextKey]) {
       visited[nextKey] = 1;
       queue.push({ floor: nextFloor, data: nextData, steps: nextSteps });
+      return true;
     }
   }
+
+  return false;
+}
+
+function emptyBelowCurrentFloor(data, currentFloor) {
+  while (currentFloor >= 0) {
+    if (data[currentFloor][0] !== 0 || data[currentFloor][1] !== 0) {
+      return false;
+    }
+
+    currentFloor--;
+  }
+
+  return true;
 }
 
 const visited = { [`0|${JSON.stringify(data)}`]: 1 };
@@ -80,11 +95,18 @@ while (queue.length) {
   const { floor, steps } = current;
 
   for (const material of materials) {
+    const hasGenerator = current.data[floor][0] & material;
+    const hasMicrochip = current.data[floor][1] & material;
+
+    if (!hasGenerator && !hasMicrochip) continue;
+
+    let doubleMove = false;
+
     // ------------------------------------------------------------
-    // Microchip + Generator Moves
+    // Two Parts Up Moves
     // ------------------------------------------------------------
-    if (current.data[floor][0] & material && current.data[floor][1] & material) {
-      if (floor < 3) {
+    if (floor < 3) {
+      if (hasGenerator && hasMicrochip) {
         // can go up
         const nextDataUp = current.data.map((f) => [...f]);
         nextDataUp[floor][0] ^= material;
@@ -92,75 +114,29 @@ while (queue.length) {
         nextDataUp[floor + 1][0] |= material;
         nextDataUp[floor + 1][1] |= material;
 
-        updateQueueIfSafe(nextDataUp, floor, floor + 1, steps + 1, queue, visited);
+        doubleMove ||= updateQueueIfSafe(nextDataUp, floor, floor + 1, steps + 1, queue, visited);
       }
 
-      if (floor > 0) {
-        // can go down
-        const nextDataDown = current.data.map((f) => [...f]);
-        nextDataDown[floor][0] ^= material;
-        nextDataDown[floor][1] ^= material;
-        nextDataDown[floor - 1][0] |= material;
-        nextDataDown[floor - 1][1] |= material;
-
-        updateQueueIfSafe(nextDataDown, floor, floor - 1, steps + 1, queue, visited);
-      }
-    }
-
-    // ------------------------------------------------------------
-    // Two Microchip Moves
-    // ------------------------------------------------------------
-    if (current.data[floor][1] & material) {
       for (const material2 of materials) {
-        if (material2 === material) continue;
+        const hasGenerator2 = current.data[floor][0] & material2;
+        const hasMicrochip2 = current.data[floor][1] & material2;
 
-        if (current.data[floor][1] & material2) {
-          if (floor < 3) {
-            // can go up
-            const nextDataUp = current.data.map((f) => [...f]);
-            nextDataUp[floor][1] ^= material | material2;
-            nextDataUp[floor + 1][1] |= material | material2;
+        if (!hasGenerator2 && !hasMicrochip2) continue;
 
-            updateQueueIfSafe(nextDataUp, floor, floor + 1, steps + 1, queue, visited);
-          }
+        if (hasMicrochip && hasMicrochip2) {
+          const nextDataUp = current.data.map((f) => [...f]);
+          nextDataUp[floor][1] ^= material | material2;
+          nextDataUp[floor + 1][1] |= material | material2;
 
-          if (floor > 0) {
-            // can go down
-            const nextDataDown = current.data.map((f) => [...f]);
-            nextDataDown[floor][1] ^= material | material2;
-            nextDataDown[floor - 1][1] |= material | material2;
-
-            updateQueueIfSafe(nextDataDown, floor, floor - 1, steps + 1, queue, visited);
-          }
+          doubleMove ||= updateQueueIfSafe(nextDataUp, floor, floor + 1, steps + 1, queue, visited);
         }
-      }
-    }
 
-    // ------------------------------------------------------------
-    // Two Generator Moves
-    // ------------------------------------------------------------
-    if (current.data[floor][0] & material) {
-      for (const material2 of materials) {
-        if (material2 === material) continue;
+        if (hasGenerator && hasGenerator2) {
+          const nextDataUp = current.data.map((f) => [...f]);
+          nextDataUp[floor][0] ^= material | material2;
+          nextDataUp[floor + 1][0] |= material | material2;
 
-        if (current.data[floor][0] & material2) {
-          if (floor < 3) {
-            // can go up
-            const nextDataUp = current.data.map((f) => [...f]);
-            nextDataUp[floor][0] ^= material | material2;
-            nextDataUp[floor + 1][0] |= material | material2;
-
-            updateQueueIfSafe(nextDataUp, floor, floor + 1, steps + 1, queue, visited);
-          }
-
-          if (floor > 0) {
-            // can go down
-            const nextDataDown = current.data.map((f) => [...f]);
-            nextDataDown[floor][0] ^= material | material2;
-            nextDataDown[floor - 1][0] |= material | material2;
-
-            updateQueueIfSafe(nextDataDown, floor, floor - 1, steps + 1, queue, visited);
-          }
+          doubleMove ||= updateQueueIfSafe(nextDataUp, floor, floor + 1, steps + 1, queue, visited);
         }
       }
     }
@@ -168,8 +144,8 @@ while (queue.length) {
     // ------------------------------------------------------------
     // Single Microchip Moves
     // ------------------------------------------------------------
-    if (current.data[floor][1] & material) {
-      if (floor < 3) {
+    if (hasMicrochip) {
+      if (floor < 3 && !doubleMove) {
         // can go up
         const nextDataUp = current.data.map((f) => [...f]);
         nextDataUp[floor][1] ^= material;
@@ -178,7 +154,7 @@ while (queue.length) {
         updateQueueIfSafe(nextDataUp, floor, floor + 1, steps + 1, queue, visited);
       }
 
-      if (floor > 0) {
+      if (floor > 0 && emptyBelowCurrentFloor(current.data, floor)) {
         // can go down
         const nextDataDown = current.data.map((f) => [...f]);
         nextDataDown[floor][1] ^= material;
@@ -191,8 +167,8 @@ while (queue.length) {
     // ------------------------------------------------------------
     // Single Generator Moves
     // ------------------------------------------------------------
-    if (current.data[floor][0] & material) {
-      if (floor < 3) {
+    if (hasGenerator) {
+      if (floor < 3 && !doubleMove) {
         // can go up
         const nextDataUp = current.data.map((f) => [...f]);
         nextDataUp[floor][0] ^= material;
--- 2016/11/part-2.js
+++ 2016/11/part-2.js
@@ -1,6 +1,6 @@
 import { read, write } from '../../utilities/io.js';
 
-const [YEAR, DAY, PART] = [2016, 11, 2];
+const [YEAR, DAY, PART] = [2016, 11, 1];
 
 const input = read(YEAR, DAY, PART);
 
@@ -58,6 +58,32 @@ function isSafe([generators, microchips]) {
   return true;
 }
 
+function updateQueueIfSafe(nextData, currentFloor, nextFloor, nextSteps, queue, visited) {
+  if (isSafe(nextData[currentFloor]) && isSafe(nextData[nextFloor])) {
+    const nextKey = `${nextFloor}|${JSON.stringify(nextData)}`;
+
+    if (!visited[nextKey]) {
+      visited[nextKey] = 1;
+      queue.push({ floor: nextFloor, data: nextData, steps: nextSteps });
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function emptyBelowCurrentFloor(data, currentFloor) {
+  while (currentFloor >= 0) {
+    if (data[currentFloor][0] !== 0 || data[currentFloor][1] !== 0) {
+      return false;
+    }
+
+    currentFloor--;
+  }
+
+  return true;
+}
+
 const visited = { [`0|${JSON.stringify(data)}`]: 1 };
 const queue = [{ floor: 0, data: data.map((f) => [...f]), steps: 0 }];
 let result = null;
@@ -74,14 +100,21 @@ while (queue.length) {
     break;
   }
 
-  const { floor } = current;
+  const { floor, steps } = current;
 
   for (const material of materials) {
+    const hasGenerator = current.data[floor][0] & material;
+    const hasMicrochip = current.data[floor][1] & material;
+
+    if (!hasGenerator && !hasMicrochip) continue;
+
+    let doubleMove = false;
+
     // ------------------------------------------------------------
-    // Microchip + Generator Moves
+    // Two Parts Up Moves
     // ------------------------------------------------------------
-    if (current.data[floor][0] & material && current.data[floor][1] & material) {
-      if (floor < 3) {
+    if (floor < 3) {
+      if (hasGenerator && hasMicrochip) {
         // can go up
         const nextDataUp = current.data.map((f) => [...f]);
         nextDataUp[floor][0] ^= material;
@@ -89,141 +122,29 @@ while (queue.length) {
         nextDataUp[floor + 1][0] |= material;
         nextDataUp[floor + 1][1] |= material;
 
-        if (isSafe(nextDataUp[floor]) && isSafe(nextDataUp[floor + 1])) {
-          const nextKey = `${floor + 1}|${JSON.stringify(nextDataUp)}`;
-
-          if (!visited[nextKey]) {
-            visited[nextKey] = 1;
-            queue.push({ floor: floor + 1, data: nextDataUp, steps: current.steps + 1 });
-          }
-        }
+        doubleMove ||= updateQueueIfSafe(nextDataUp, floor, floor + 1, steps + 1, queue, visited);
       }
 
-      if (floor > 0) {
-        // can go down
-        const nextDataDown = current.data.map((f) => [...f]);
-        nextDataDown[floor][0] ^= material;
-        nextDataDown[floor][1] ^= material;
-        nextDataDown[floor - 1][0] |= material;
-        nextDataDown[floor - 1][1] |= material;
-
-        if (isSafe(nextDataDown[floor]) && isSafe(nextDataDown[floor - 1])) {
-          const nextKey = `${floor - 1}|${JSON.stringify(nextDataDown)}`;
-
-          if (!visited[nextKey]) {
-            visited[nextKey] = 1;
-            queue.push({ floor: floor - 1, data: nextDataDown, steps: current.steps + 1 });
-          }
-        }
-      }
-    }
-
-    // ------------------------------------------------------------
-    // Two Microchip Moves
-    // ------------------------------------------------------------
-    if (current.data[floor][1] & material) {
       for (const material2 of materials) {
-        if (material2 === material) continue;
+        const hasGenerator2 = current.data[floor][0] & material2;
+        const hasMicrochip2 = current.data[floor][1] & material2;
 
-        if (current.data[floor][1] & material2) {
-          if (floor < 3) {
-            // can go up
-            const nextDataUp = current.data.map((f) => [...f]);
-            nextDataUp[floor][1] ^= material;
-            nextDataUp[floor][1] ^= material2;
-            nextDataUp[floor + 1][1] |= material;
-            nextDataUp[floor + 1][1] |= material2;
+        if (!hasGenerator2 && !hasMicrochip2) continue;
 
-            if (isSafe(nextDataUp[floor]) && isSafe(nextDataUp[floor + 1])) {
-              const nextKey = `${floor + 1}|${JSON.stringify(nextDataUp)}`;
+        if (hasMicrochip && hasMicrochip2) {
+          const nextDataUp = current.data.map((f) => [...f]);
+          nextDataUp[floor][1] ^= material | material2;
+          nextDataUp[floor + 1][1] |= material | material2;
 
-              if (!visited[nextKey]) {
-                visited[nextKey] = 1;
-                queue.push({
-                  floor: floor + 1,
-                  data: nextDataUp,
-                  steps: current.steps + 1,
-                });
-              }
-            }
-          }
-
-          if (floor > 0) {
-            // can go down
-            const nextDataDown = current.data.map((f) => [...f]);
-            nextDataDown[floor][1] ^= material;
-            nextDataDown[floor][1] ^= material2;
-            nextDataDown[floor - 1][1] |= material;
-            nextDataDown[floor - 1][1] |= material2;
-
-            if (isSafe(nextDataDown[floor]) && isSafe(nextDataDown[floor - 1])) {
-              const nextKey = `${floor - 1}|${JSON.stringify(nextDataDown)}`;
-
-              if (!visited[nextKey]) {
-                visited[nextKey] = 1;
-                queue.push({
-                  floor: floor - 1,
-                  data: nextDataDown,
-                  steps: current.steps + 1,
-                });
-              }
-            }
-          }
+          doubleMove ||= updateQueueIfSafe(nextDataUp, floor, floor + 1, steps + 1, queue, visited);
         }
-      }
-    }
 
-    // ------------------------------------------------------------
-    // Two Generator Moves
-    // ------------------------------------------------------------
-    if (current.data[floor][0] & material) {
-      for (const material2 of materials) {
-        if (material2 === material) continue;
+        if (hasGenerator && hasGenerator2) {
+          const nextDataUp = current.data.map((f) => [...f]);
+          nextDataUp[floor][0] ^= material | material2;
+          nextDataUp[floor + 1][0] |= material | material2;
 
-        if (current.data[floor][0] & material2) {
-          if (floor < 3) {
-            // can go up
-            const nextDataUp = current.data.map((f) => [...f]);
-            nextDataUp[floor][0] ^= material;
-            nextDataUp[floor][0] ^= material2;
-            nextDataUp[floor + 1][0] |= material;
-            nextDataUp[floor + 1][0] |= material2;
-
-            if (isSafe(nextDataUp[floor]) && isSafe(nextDataUp[floor + 1])) {
-              const nextKey = `${floor + 1}|${JSON.stringify(nextDataUp)}`;
-
-              if (!visited[nextKey]) {
-                visited[nextKey] = 1;
-                queue.push({
-                  floor: floor + 1,
-                  data: nextDataUp,
-                  steps: current.steps + 1,
-                });
-              }
-            }
-          }
-
-          if (floor > 0) {
-            // can go down
-            const nextDataDown = current.data.map((f) => [...f]);
-            nextDataDown[floor][0] ^= material;
-            nextDataDown[floor][0] ^= material2;
-            nextDataDown[floor - 1][0] |= material;
-            nextDataDown[floor - 1][0] |= material2;
-
-            if (isSafe(nextDataDown[floor]) && isSafe(nextDataDown[floor - 1])) {
-              const nextKey = `${floor - 1}|${JSON.stringify(nextDataDown)}`;
-
-              if (!visited[nextKey]) {
-                visited[nextKey] = 1;
-                queue.push({
-                  floor: floor - 1,
-                  data: nextDataDown,
-                  steps: current.steps + 1,
-                });
-              }
-            }
-          }
+          doubleMove ||= updateQueueIfSafe(nextDataUp, floor, floor + 1, steps + 1, queue, visited);
         }
       }
     }
@@ -231,58 +152,37 @@ while (queue.length) {
     // ------------------------------------------------------------
     // Single Microchip Moves
     // ------------------------------------------------------------
-    if (current.data[floor][1] & material) {
-      if (floor < 3) {
+    if (hasMicrochip) {
+      if (floor < 3 && !doubleMove) {
         // can go up
         const nextDataUp = current.data.map((f) => [...f]);
         nextDataUp[floor][1] ^= material;
         nextDataUp[floor + 1][1] |= material;
 
-        if (isSafe(nextDataUp[floor]) && isSafe(nextDataUp[floor + 1])) {
-          const nextKey = `${floor + 1}|${JSON.stringify(nextDataUp)}`;
-
-          if (!visited[nextKey]) {
-            visited[nextKey] = 1;
-            queue.push({ floor: floor + 1, data: nextDataUp, steps: current.steps + 1 });
-          }
-        }
+        updateQueueIfSafe(nextDataUp, floor, floor + 1, steps + 1, queue, visited);
       }
 
-      if (floor > 0) {
+      if (floor > 0 && emptyBelowCurrentFloor(current.data, floor)) {
         // can go down
         const nextDataDown = current.data.map((f) => [...f]);
         nextDataDown[floor][1] ^= material;
         nextDataDown[floor - 1][1] |= material;
 
-        if (isSafe(nextDataDown[floor]) && isSafe(nextDataDown[floor - 1])) {
-          const nextKey = `${floor - 1}|${JSON.stringify(nextDataDown)}`;
-
-          if (!visited[nextKey]) {
-            visited[nextKey] = 1;
-            queue.push({ floor: floor - 1, data: nextDataDown, steps: current.steps + 1 });
-          }
-        }
+        updateQueueIfSafe(nextDataDown, floor, floor - 1, steps + 1, queue, visited);
       }
     }
 
     // ------------------------------------------------------------
     // Single Generator Moves
     // ------------------------------------------------------------
-    if (current.data[floor][0] & material) {
-      if (floor < 3) {
+    if (hasGenerator) {
+      if (floor < 3 && !doubleMove) {
         // can go up
         const nextDataUp = current.data.map((f) => [...f]);
         nextDataUp[floor][0] ^= material;
         nextDataUp[floor + 1][0] |= material;
 
-        if (isSafe(nextDataUp[floor]) && isSafe(nextDataUp[floor + 1])) {
-          const nextKey = `${floor + 1}|${JSON.stringify(nextDataUp)}`;
-
-          if (!visited[nextKey]) {
-            visited[nextKey] = 1;
-            queue.push({ floor: floor + 1, data: nextDataUp, steps: current.steps + 1 });
-          }
-        }
+        updateQueueIfSafe(nextDataUp, floor, floor + 1, steps + 1, queue, visited);
       }
 
       if (floor > 0) {
@@ -291,19 +191,10 @@ while (queue.length) {
         nextDataDown[floor][0] ^= material;
         nextDataDown[floor - 1][0] |= material;
 
-        if (isSafe(nextDataDown[floor]) && isSafe(nextDataDown[floor - 1])) {
-          const nextKey = `${floor - 1}|${JSON.stringify(nextDataDown)}`;
-
-          if (!visited[nextKey]) {
-            visited[nextKey] = 1;
-            queue.push({ floor: floor - 1, data: nextDataDown, steps: current.steps + 1 });
-          }
-        }
+        updateQueueIfSafe(nextDataDown, floor, floor - 1, steps + 1, queue, visited);
       }
     }
   }
 }
 
 write(YEAR, DAY, PART, result);
-
-// TODO: Come back and improve performance
