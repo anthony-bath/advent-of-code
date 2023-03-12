507afdaba375c1f19ec68d21a25c08681821b663
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-12 15:45:59 -0400

    2016 Day 18 Complete

---
+++ 2016/18/part-1.js
@@ -0,0 +1,35 @@
+import { printGrid } from '../../utilities/grid.js';
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2016, 18, 1];
+
+const input = read(YEAR, DAY, PART, { splitBy: '' });
+
+const trapPatterns = ['^^.', '.^^', '^..', '..^'];
+
+let safeCount = input.filter((t) => t === '.').length;
+const rows = [input];
+let prevRowIndex = 0;
+
+while (rows.length < 40) {
+  const nextRow = [];
+  const prevRow = rows[prevRowIndex];
+
+  for (let tile = 0; tile < prevRow.length; tile++) {
+    const left = tile - 1 >= 0 ? prevRow[tile - 1] : '.';
+    const center = prevRow[tile];
+    const right = tile + 1 < prevRow.length ? prevRow[tile + 1] : '.';
+
+    if (trapPatterns.includes(`${left}${center}${right}`)) {
+      nextRow.push('^');
+    } else {
+      safeCount++;
+      nextRow.push('.');
+    }
+  }
+
+  rows.push(nextRow);
+  prevRowIndex++;
+}
+
+write(YEAR, DAY, PART, safeCount);
---
+++ 2016/18/part-2.js
@@ -0,0 +1,36 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2016, 18, 2];
+
+const input = read(YEAR, DAY, PART, { splitBy: '' });
+
+const TRAP_PATTERNS = ['^^.', '.^^', '^..', '..^'];
+const TARGET_ROWS = 400000;
+
+let safeCount = input.filter((t) => t === '.').length;
+let prevRow = ['.', ...input, '.'];
+let prevRowIndex = 0;
+
+while (prevRowIndex < TARGET_ROWS - 1) {
+  const nextRow = ['.'];
+
+  for (let tile = 1; tile < prevRow.length - 1; tile++) {
+    const left = prevRow[tile - 1];
+    const center = prevRow[tile];
+    const right = prevRow[tile + 1];
+
+    if (TRAP_PATTERNS.includes(`${left}${center}${right}`)) {
+      nextRow.push('^');
+    } else {
+      nextRow.push('.');
+      safeCount++;
+    }
+  }
+
+  nextRow.push('.');
+
+  prevRow = nextRow;
+  prevRowIndex++;
+}
+
+write(YEAR, DAY, PART, safeCount);
