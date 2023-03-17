36c9a9934e3291e39b56b4f177c93afe5b1849b6
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-17 12:43:59 -0400

    2017 Day 9 Complete

---
+++ 2017/09/part-1.js
@@ -0,0 +1,26 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2017, 9, 1];
+
+const expr = /(!.)/g;
+
+const input = read(YEAR, DAY, PART, { splitBy: null, test: true }).replaceAll(expr, '').split('');
+
+let inGarbage = false;
+let depth = 0;
+let score = 0;
+
+for (const item of input) {
+  if (item === '{' && !inGarbage) {
+    depth++;
+  } else if (item === '<' && !inGarbage) {
+    inGarbage = true;
+  } else if (item === '>' && inGarbage) {
+    inGarbage = false;
+  } else if (item === '}' && !inGarbage) {
+    score += depth;
+    depth--;
+  }
+}
+
+write(YEAR, DAY, PART, score);
---
+++ 2017/09/part-2.js
@@ -0,0 +1,28 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2017, 9, 2];
+
+const expr = /(!.)/g;
+
+const input = read(YEAR, DAY, PART, { splitBy: null }).replaceAll(expr, '').split('');
+
+let inGarbage = false;
+let count = 0;
+
+for (const item of input) {
+  if (inGarbage && item !== '>') {
+    count++;
+    continue;
+  }
+
+  if (item === '<') {
+    inGarbage = true;
+    continue;
+  }
+
+  if (item === '>') {
+    inGarbage = false;
+  }
+}
+
+write(YEAR, DAY, PART, count);
