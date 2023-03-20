10242436998614002c21113acc559e5611b50cb8
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-20 07:43:29 -0400

    2015 Day 3 Complete

---
+++ 2015/03/part-2.js
@@ -0,0 +1,28 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2015, 3, 2];
+
+const delta = { v: [0, 1], '^': [0, -1], '<': [-1, 0], '>': [1, 0] };
+const presentsByCoord = { '0|0': 2 };
+const santas = { [true]: [0, 0], [false]: [0, 0] };
+let flip = true;
+
+read(YEAR, DAY, PART, { splitBy: '' }).forEach((dir) => {
+  const [dx, dy] = delta[dir];
+  const [x, y] = santas[flip];
+  const nextX = x + dx;
+  const nextY = y + dy;
+
+  santas[flip] = [nextX, nextY];
+  const nextKey = `${nextX}|${nextY}`;
+
+  if (!(nextKey in presentsByCoord)) {
+    presentsByCoord[nextKey] = 1;
+  } else {
+    presentsByCoord[nextKey]++;
+  }
+
+  flip = !flip;
+});
+
+write(YEAR, DAY, PART, Object.keys(presentsByCoord).length);
