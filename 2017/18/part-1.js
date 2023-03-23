788bba9a37106f3c97acb63517b2be6914eda77d
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-23 08:08:17 -0400

    2017 Day 18 Part 1 Complete

---
+++ 2017/18/part-1.js
@@ -0,0 +1,90 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2017, 18, 1];
+
+const instructions = read(YEAR, DAY, PART).map((line) => line.split(' '));
+
+const registers = new Map();
+
+function getValue(arg) {
+  if (/[a-z]/.test(arg)) {
+    if (!registers.has(arg)) {
+      registers.set(arg, 0);
+    }
+
+    return registers.get(arg);
+  }
+
+  return Number(arg);
+}
+
+let pointer = 0;
+let result = null;
+let played = [];
+
+while (true) {
+  const [command, arg1, arg2] = instructions[pointer];
+
+  switch (command) {
+    case 'set':
+      {
+        registers.set(arg1, getValue(arg2));
+        pointer++;
+      }
+      break;
+
+    case 'add':
+      {
+        registers.set(arg1, (registers.get(arg1) ?? 0) + getValue(arg2));
+        pointer++;
+      }
+      break;
+
+    case 'mul':
+      {
+        registers.set(arg1, (registers.get(arg1) ?? 0) * getValue(arg2));
+        pointer++;
+      }
+      break;
+
+    case 'mod':
+      {
+        registers.set(arg1, (registers.get(arg1) ?? 0) % getValue(arg2));
+        pointer++;
+      }
+      break;
+
+    case 'rcv':
+      {
+        const recovered = getValue(arg1);
+
+        if (recovered !== 0 && !result) {
+          result = played.pop();
+        }
+
+        pointer++;
+      }
+      break;
+
+    case 'jgz':
+      {
+        if (getValue(arg1) > 0) {
+          pointer += getValue(arg2);
+        } else {
+          pointer++;
+        }
+      }
+      break;
+
+    case 'snd': {
+      played.push(getValue(arg1));
+      pointer++;
+    }
+  }
+
+  if (pointer >= instructions.length || result) {
+    break;
+  }
+}
+
+write(YEAR, DAY, PART, result);
