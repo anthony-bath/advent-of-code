257daafdd3abae8b6bf31dc5b67881edb7e49709
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-25 16:15:12 -0400

    2017 Day 22 Part 1 Complete

---
+++ 2017/22/part-1.js
@@ -0,0 +1,66 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2017, 22, 1];
+
+const grid = new Map();
+
+read(YEAR, DAY, PART).forEach((line, y) => {
+  line.split('').map((cell, x) => grid.set(`${x}|${y}`, cell));
+});
+
+function getGridValue(x, y) {
+  const key = `${x}|${y}`;
+
+  if (!grid.has(key)) {
+    grid.set(key, '.');
+  }
+
+  return grid.get(key);
+}
+
+const DIR = {
+  NORTH: 0,
+  EAST: 1,
+  SOUTH: 2,
+  WEST: 3,
+};
+
+const BURSTS = 10000;
+
+let [x, y] = [12, 12];
+let direction = DIR.NORTH;
+let count = 0;
+
+for (let burst = 0; burst < BURSTS; burst++) {
+  const key = `${x}|${y}`;
+  const current = getGridValue(x, y);
+
+  if (current === '#') {
+    direction = (direction + 1) % 4;
+    grid.set(key, '.');
+  } else {
+    direction = direction - 1 >= 0 ? direction - 1 : DIR.WEST;
+    grid.set(key, '#');
+    count++;
+  }
+
+  switch (direction) {
+    case DIR.NORTH:
+      y--;
+      break;
+
+    case DIR.EAST:
+      x++;
+      break;
+
+    case DIR.SOUTH:
+      y++;
+      break;
+
+    case DIR.WEST:
+      x--;
+      break;
+  }
+}
+
+write(YEAR, DAY, PART, count);
