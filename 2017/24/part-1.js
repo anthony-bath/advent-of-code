0a8c73727739871f75c6ad50d7f9b7e61801de72
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-26 13:42:13 -0400

    2017 Day 24 Complete

---
+++ 2017/24/part-1.js
@@ -0,0 +1,32 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2017, 24, 1];
+
+class Component {
+  constructor([left, right]) {
+    this.used = false;
+    this.left = left;
+    this.right = right;
+    this.strength = left + right;
+  }
+}
+
+const components = read(YEAR, DAY, PART).map(
+  (line) => new Component(line.split('/').map((n) => Number(n)))
+);
+
+function dfs(connector, strength) {
+  const strengths = [strength];
+
+  for (const c of components) {
+    if (!c.used && (c.left === connector || c.right === connector)) {
+      c.used = true;
+      strengths.push(dfs(c.left === connector ? c.right : c.left, strength + c.strength));
+      c.used = false;
+    }
+  }
+
+  return Math.max(...strengths);
+}
+
+write(YEAR, DAY, PART, dfs(0, 0));
---
+++ 2017/24/part-2.js
@@ -0,0 +1,39 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2017, 24, 2];
+
+class Component {
+  constructor([left, right]) {
+    this.used = false;
+    this.left = left;
+    this.right = right;
+    this.strength = left + right;
+  }
+}
+
+const components = read(YEAR, DAY, PART).map(
+  (line) => new Component(line.split('/').map((n) => Number(n)))
+);
+
+let maxLength = -Infinity;
+let maxStrengthOfLongest = -Infinity;
+
+function dfs(connector, strength, length) {
+  maxLength = Math.max(length, maxLength);
+
+  if (length === maxLength) {
+    maxStrengthOfLongest = Math.max(maxStrengthOfLongest, strength);
+  }
+
+  for (const c of components) {
+    if (!c.used && (c.left === connector || c.right === connector)) {
+      c.used = true;
+      dfs(c.left === connector ? c.right : c.left, strength + c.strength, length + 1);
+      c.used = false;
+    }
+  }
+
+  return maxStrengthOfLongest;
+}
+
+write(YEAR, DAY, PART, dfs(0, 0, 0));
