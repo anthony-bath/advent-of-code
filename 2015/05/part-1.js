be522e42f03b469854622baf135281f28b46d0b3
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-27 12:40:25 -0400

    2015 Day 5 Part 1 Complete

---
+++ 2015/05/part-1.js
@@ -0,0 +1,17 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2015, 5, 1];
+
+const vowels = /[aeiou]/g;
+const double = /([a-z])\1/;
+const ignore = /(ab|cd|pq|xy)/;
+
+let niceCount = 0;
+
+read(YEAR, DAY, PART).forEach((string) => {
+  if (string.match(vowels)?.length >= 3 && double.test(string) && !ignore.test(string)) {
+    niceCount++;
+  }
+});
+
+write(YEAR, DAY, PART, niceCount);
