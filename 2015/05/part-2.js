2c71337f075f82b1cf5ea6049b95f0798e835d15
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-28 07:59:04 -0400

    2015 Day 5 Complete

---
+++ 2015/05/part-2.js
@@ -0,0 +1,16 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2015, 5, 2];
+
+const double = /([a-z][a-z]).*\1/;
+const triple = /([a-z])[a-z]\1/;
+
+let niceCount = 0;
+
+read(YEAR, DAY, PART).forEach((string) => {
+  if (double.test(string) && triple.test(string)) {
+    niceCount++;
+  }
+});
+
+write(YEAR, DAY, PART, niceCount);
