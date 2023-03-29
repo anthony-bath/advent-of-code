ab3345c9f89c9e498a465946d3d471f64936ecf3
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-29 07:25:20 -0400

    2015 Day 6 Part 1 Complete

---
+++ 2015/06/part-1.js
@@ -0,0 +1,33 @@
+import { read, write } from '../../utilities/io.js';
+
+const grid = Array(1000)
+  .fill()
+  .map(() => Array(1000).fill(false));
+
+const [YEAR, DAY, PART] = [2015, 6, 1];
+
+read(YEAR, DAY, PART).forEach((line) => {
+  const [x1, y1, x2, y2] = line.match(/\d+/g).map((n) => Number(n));
+
+  for (let x = x1; x <= x2; x++) {
+    for (let y = y1; y <= y2; y++) {
+      if (line.startsWith('turn on')) {
+        grid[y][x] = true;
+      } else if (line.startsWith('turn off')) {
+        grid[y][x] = false;
+      } else {
+        grid[y][x] = !grid[y][x];
+      }
+    }
+  }
+});
+
+let count = 0;
+
+for (let x = 0; x < 1000; x++) {
+  for (let y = 0; y < 1000; y++) {
+    if (grid[y][x]) count++;
+  }
+}
+
+write(YEAR, DAY, PART, count);
