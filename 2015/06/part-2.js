8f3134fe17e931364b520c9e59e4a547e6d27ee1
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-03-30 07:21:34 -0400

    2015 Day 6 Complete

--- 2015/06/part-2.js
+++ 2015/06/part-2.js
@@ -1,10 +1,10 @@
 import { read, write } from '../../utilities/io.js';
 
-const [YEAR, DAY, PART] = [2015, 6, 1];
+const [YEAR, DAY, PART] = [2015, 6, 2];
 
 const grid = Array(1000)
   .fill()
-  .map(() => Array(1000).fill(false));
+  .map(() => Array(1000).fill(0));
 
 read(YEAR, DAY, PART).forEach((line) => {
   const [x1, y1, x2, y2] = line.match(/\d+/g).map((n) => Number(n));
@@ -12,22 +12,22 @@ read(YEAR, DAY, PART).forEach((line) => {
   for (let x = x1; x <= x2; x++) {
     for (let y = y1; y <= y2; y++) {
       if (line.startsWith('turn on')) {
-        grid[y][x] = true;
+        grid[y][x]++;
       } else if (line.startsWith('turn off')) {
-        grid[y][x] = false;
+        grid[y][x] = Math.max(0, grid[y][x] - 1);
       } else {
-        grid[y][x] = !grid[y][x];
+        grid[y][x] += 2;
       }
     }
   }
 });
 
-let count = 0;
+let brightness = 0;
 
 for (let x = 0; x < 1000; x++) {
   for (let y = 0; y < 1000; y++) {
-    if (grid[y][x]) count++;
+    brightness += grid[y][x];
   }
 }
 
-write(YEAR, DAY, PART, count);
+write(YEAR, DAY, PART, brightness);
