5dcd6cea978b3c016407258c5efc0af03140100c
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-04-02 14:31:36 -0400

    2015 Day 8 Part 1 Complete

---
+++ 2015/08/part-1.js
@@ -0,0 +1,28 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2015, 8, 1];
+
+let result = 0;
+
+read(YEAR, DAY, PART).forEach((line) => {
+  const initial = line.length;
+  line = line.substring(1, line.length - 1);
+
+  const matches = line.match(/(\\.?x[0-9a-f]{2})/g);
+
+  if (matches) {
+    for (const match of matches) {
+      const code = match.slice(-2);
+      line = line.replace(match, String.fromCharCode(parseInt(code, 16)));
+    }
+  }
+
+  line = line.replace(/\\"/g, '"');
+  line = line.replace(/\\\\/g, '\\');
+
+  const final = line.length;
+
+  result += initial - final;
+});
+
+write(YEAR, DAY, PART, result);
