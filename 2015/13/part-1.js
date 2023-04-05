e7ffa1aea7f7558f9b6a2630aa1cf81f6d97b963
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-04-05 07:57:42 -0400

    2015 Day 13 Complete

---
+++ 2015/13/part-1.js
@@ -0,0 +1,44 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2015, 13, 1];
+
+const expr =
+  /(?<person>\w+) would (?<type>(gain|lose)) (?<diff>\d+) happiness units by sitting next to (?<next>\w+)./;
+
+const people = new Map();
+
+read(YEAR, DAY, PART).forEach((line) => {
+  const { person, type, diff, next } = line.match(expr).groups;
+
+  if (!people.has(person)) {
+    people.set(person, { [next]: Number(`${type === 'lose' ? '-' : ''}${diff}`) });
+  } else {
+    people.get(person)[next] = Number(`${type === 'lose' ? '-' : ''}${diff}`);
+  }
+});
+
+let maxChange = -Infinity;
+const queue = [{ change: 0, order: [[...people.keys()].pop()] }];
+
+while (queue.length) {
+  const current = queue.shift();
+  const currentPerson = current.order.slice(-1)[0];
+
+  if (current.order.length === people.size) {
+    current.change +=
+      people.get(currentPerson)[current.order[0]] + people.get(current.order[0])[currentPerson];
+
+    maxChange = Math.max(current.change, maxChange);
+    continue;
+  }
+
+  const data = people.get(current.order[current.order.length - 1]);
+  const choices = Object.keys(data).filter((person) => !current.order.includes(person));
+
+  for (const person of choices) {
+    const change = data[person] + people.get(person)[currentPerson];
+    queue.push({ change: current.change + change, order: [...current.order, person] });
+  }
+}
+
+write(YEAR, DAY, PART, maxChange);
---
+++ 2015/13/part-2.js
@@ -0,0 +1,55 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2015, 13, 2];
+
+const expr =
+  /(?<person>\w+) would (?<type>(gain|lose)) (?<diff>\d+) happiness units by sitting next to (?<next>\w+)./;
+
+const people = new Map();
+
+read(YEAR, DAY, PART).forEach((line) => {
+  const { person, type, diff, next } = line.match(expr).groups;
+
+  if (!people.has(person)) {
+    people.set(person, { [next]: Number(`${type === 'lose' ? '-' : ''}${diff}`) });
+  } else {
+    people.get(person)[next] = Number(`${type === 'lose' ? '-' : ''}${diff}`);
+  }
+});
+
+for (const [_, data] of people) {
+  data['Me'] = 0;
+}
+
+people.set(
+  'Me',
+  [...people.keys()].reduce((data, person) => ({ ...data, [person]: 0 }), {})
+);
+
+let maxChange = -Infinity;
+
+for (const person of people.keys()) {
+  if (person === 'Me') continue;
+
+  const queue = [{ change: 0, order: ['Me', person] }];
+
+  while (queue.length) {
+    const current = queue.shift();
+    const currentPerson = current.order.slice(-1)[0];
+
+    if (current.order.length === people.size) {
+      maxChange = Math.max(current.change, maxChange);
+      continue;
+    }
+
+    const data = people.get(current.order[current.order.length - 1]);
+    const choices = Object.keys(data).filter((person) => !current.order.includes(person));
+
+    for (const person of choices) {
+      const change = data[person] + people.get(person)[currentPerson];
+      queue.push({ change: current.change + change, order: [...current.order, person] });
+    }
+  }
+}
+
+write(YEAR, DAY, PART, maxChange);
