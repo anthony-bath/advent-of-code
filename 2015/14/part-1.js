318c97452276a8fbe06ff317b05fd845ab0c5935
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-04-06 07:58:32 -0400

    2015 Day 14 Complete

---
+++ 2015/14/part-1.js
@@ -0,0 +1,34 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2015, 14, 1];
+
+class Reindeer {
+  constructor(name, speed, travelTime, restTime) {
+    this.name = name;
+    this.speed = speed;
+    this.travelTime = travelTime;
+    this.restTime = restTime;
+    this.cycleTime = travelTime + restTime;
+  }
+
+  distanceTraveled(time) {
+    const completeCycles = Math.floor(time / this.cycleTime);
+    const remainingTime = time % this.cycleTime;
+
+    return (
+      completeCycles * this.travelTime * this.speed +
+      Math.min(this.travelTime, remainingTime) * this.speed
+    );
+  }
+}
+
+const reindeers = [];
+
+read(YEAR, DAY, PART).forEach((line) => {
+  const [name] = line.split(' ');
+  const [speed, travelTime, restTime] = line.match(/\d+/g).map((n) => Number(n));
+
+  reindeers.push(new Reindeer(name, speed, travelTime, restTime));
+});
+
+write(YEAR, DAY, PART, Math.max(...reindeers.map((reindeer) => reindeer.distanceTraveled(2503))));
---
+++ 2015/14/part-2.js
@@ -0,0 +1,57 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2015, 14, 2];
+
+class Reindeer {
+  constructor(name, speed, travelTime, restTime) {
+    this.name = name;
+    this.speed = speed;
+    this.travelTime = travelTime;
+    this.restTime = restTime;
+    this.points = 0;
+    this.cycleTime = travelTime + restTime;
+  }
+
+  distanceTraveled(time) {
+    const completeCycles = Math.floor(time / this.cycleTime);
+    const remainingTime = time % this.cycleTime;
+
+    return (
+      completeCycles * this.travelTime * this.speed +
+      Math.min(this.travelTime, remainingTime) * this.speed
+    );
+  }
+
+  score() {
+    this.points++;
+  }
+}
+
+const reindeers = new Map();
+
+read(YEAR, DAY, PART).forEach((line) => {
+  const [name] = line.split(' ');
+  const [speed, travelTime, restTime] = line.match(/\d+/g).map((n) => Number(n));
+
+  reindeers.set(name, new Reindeer(name, speed, travelTime, restTime));
+});
+
+for (let time = 1; time <= 2503; time++) {
+  const distanceData = [...reindeers.values()].map((reindeer) => ({
+    name: reindeer.name,
+    distance: reindeer.distanceTraveled(time),
+  }));
+
+  distanceData.sort((a, b) => b.distance - a.distance);
+  reindeers.get(distanceData[0].name).score();
+
+  for (let i = 1; i < distanceData.length; i++) {
+    if (distanceData[i].distance === distanceData[0].distance) {
+      reindeers.get(distanceData[i].name).score();
+    } else {
+      break;
+    }
+  }
+}
+
+write(YEAR, DAY, PART, [...reindeers.values()].sort((a, b) => b.points - a.points).shift().points);
