7ac0ec2542bfadd8b9e38ef72fbac7425d356b26
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-04-16 16:07:34 -0400

    2018 Day 22 Complete

---
+++ 2018/22/part-1.js
@@ -0,0 +1,48 @@
+import { sum } from '../../utilities/array.js';
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2018, 22, 1];
+
+const [depth, [tx, ty]] = read(YEAR, DAY, PART).map((line) => {
+  if (line.startsWith('depth')) {
+    return Number(line.match(/\d+/));
+  } else {
+    return line.match(/\d+/g).map((n) => Number(n));
+  }
+});
+
+const erosionLevels = Array(ty + 1)
+  .fill()
+  .map(() => Array(tx + 1).fill(0));
+
+const regionTypes = Array(ty + 1)
+  .fill()
+  .map(() => Array(tx + 1).fill(0));
+
+for (let y = 0; y < erosionLevels.length; y++) {
+  for (let x = 0; x < erosionLevels[0].length; x++) {
+    let geologicIndex;
+
+    if (x === 0 && y === 0) {
+      geologicIndex = 0;
+    } else if (x === tx && y === ty) {
+      geologicIndex = 0;
+    } else if (y === 0) {
+      geologicIndex = x * 16807;
+    } else if (x === 0) {
+      geologicIndex = y * 48271;
+    } else {
+      geologicIndex = erosionLevels[y - 1][x] * erosionLevels[y][x - 1];
+    }
+
+    erosionLevels[y][x] = (geologicIndex + depth) % 20183;
+    regionTypes[y][x] = erosionLevels[y][x] % 3;
+  }
+}
+
+write(
+  YEAR,
+  DAY,
+  PART,
+  regionTypes.reduce((total, row) => total + sum(row), 0)
+);
---
+++ 2018/22/part-2.js
@@ -0,0 +1,162 @@
+import { read, write } from '../../utilities/io.js';
+import { manhattan } from '../../utilities/math.js';
+
+const [YEAR, DAY, PART] = [2018, 22, 1];
+
+const [depth, [tx, ty]] = read(YEAR, DAY, PART).map((line) => {
+  if (line.startsWith('depth')) {
+    return Number(line.match(/\d+/));
+  } else {
+    return line.match(/\d+/g).map((n) => Number(n));
+  }
+});
+
+const erosionLevels = new Map();
+
+function setErosionLevel(x, y) {
+  let geologicIndex = 0;
+
+  if (x === 0 && y === 0) {
+    geologicIndex = 0;
+  } else if (x === tx && y === ty) {
+    geologicIndex = 0;
+  } else if (y === 0) {
+    geologicIndex = x * 16807;
+  } else if (x === 0) {
+    geologicIndex = y * 48271;
+  } else {
+    const adjKey1 = `${x}|${y - 1}`;
+    const adjKey2 = `${x - 1}|${y}`;
+
+    if (!erosionLevels.has(adjKey1)) {
+      setErosionLevel(x, y - 1);
+    }
+
+    if (!erosionLevels.has(adjKey2)) {
+      setErosionLevel(x - 1, y);
+    }
+
+    geologicIndex = erosionLevels.get(adjKey1) * erosionLevels.get(adjKey2);
+  }
+
+  erosionLevels.set(`${x}|${y}`, (geologicIndex + depth) % 20183);
+}
+
+function getRegionType(x, y) {
+  const key = `${x}|${y}`;
+
+  if (!erosionLevels.has(key)) {
+    setErosionLevel(x, y);
+  }
+
+  return erosionLevels.get(key) % 3;
+}
+
+const REGION = {
+  ROCKY: 0,
+  WET: 1,
+  NARROW: 2,
+};
+
+const ITEM = {
+  NEITHER: 0,
+  TORCH: 1,
+  CLIMBING_GEAR: 2,
+};
+
+const ALL_ITEMS = [ITEM.NEITHER, ITEM.TORCH, ITEM.CLIMBING_GEAR];
+
+const VALID_ITEMS_BY_REGION = {
+  [REGION.ROCKY]: [ITEM.TORCH, ITEM.CLIMBING_GEAR],
+  [REGION.WET]: [ITEM.NEITHER, ITEM.CLIMBING_GEAR],
+  [REGION.NARROW]: [ITEM.TORCH, ITEM.NEITHER],
+};
+
+function isValid(item, currentRegion, targetRegion) {
+  return (
+    VALID_ITEMS_BY_REGION[currentRegion].includes(item) &&
+    VALID_ITEMS_BY_REGION[targetRegion].includes(item)
+  );
+}
+
+function insertIntoSortedQueue(queue, state) {
+  let low = 0;
+  let high = queue.length;
+
+  while (low < high) {
+    let mid = (low + high) >>> 1;
+
+    if (queue[mid].minutes > state.minutes) {
+      low = mid + 1;
+    } else {
+      high = mid;
+    }
+  }
+
+  queue.splice(low, 0, state);
+}
+
+const deltas = [
+  [0, 1],
+  [1, 0],
+  [-1, 0],
+  [0, -1],
+];
+
+let result = null;
+const target = { x: tx, y: ty };
+const maxDistance = manhattan({ x: 0, y: 0 }, target);
+const state = { minutes: 0, x: 0, y: 0, item: ITEM.TORCH, distance: maxDistance };
+const visited = new Map();
+const queue = [state];
+
+while (queue.length) {
+  const current = queue.pop();
+
+  if (current.x === tx && current.y === ty && current.item === ITEM.TORCH) {
+    result = current.minutes;
+    break;
+  }
+
+  const key = `${current.item}|${current.x}|${current.y}`;
+
+  if (!visited.has(key) || visited.get(key) > current.minutes) {
+    visited.set(key, current.minutes);
+  } else {
+    // Been here before at less minutes, no need to search
+    continue;
+  }
+
+  if (current.distance > maxDistance || current.x > 10 * tx) {
+    continue;
+  }
+
+  for (const [dx, dy] of deltas) {
+    const nextX = current.x + dx;
+    const nextY = current.y + dy;
+
+    if (nextX < 0 || nextY < 0) {
+      continue;
+    }
+
+    for (const item of ALL_ITEMS) {
+      if (isValid(item, getRegionType(current.x, current.y), getRegionType(nextX, nextY))) {
+        const distance = manhattan({ x: nextX, y: nextY }, target);
+
+        insertIntoSortedQueue(queue, {
+          distance,
+          item,
+          minutes: current.minutes + (item === current.item ? 1 : 8),
+          x: nextX,
+          y: nextY,
+        });
+      }
+    }
+  }
+}
+
+write(YEAR, DAY, PART, result);
+
+// 1451 - Too High
+// 1345 - Wrong
+// 994 - Wrong
