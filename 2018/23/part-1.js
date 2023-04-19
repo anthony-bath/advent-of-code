d07470ca50085791dbcfee4532e5d6b9ede631f6
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-04-19 08:54:52 -0400

    2018 Day 23 Part 1 Complete

---
+++ 2018/23/part-1.js
@@ -0,0 +1,28 @@
+import { read, write } from '../../utilities/io.js';
+import { Point3D, manhattan3D } from '../../utilities/math.js';
+
+const [YEAR, DAY, PART] = [2018, 23, 1];
+
+class Robot {
+  constructor(location, radius) {
+    this.location = location;
+    this.radius = radius;
+  }
+}
+
+const robots = read(YEAR, DAY, PART)
+  .map((line) => {
+    const [x, y, z, r] = line.match(/-?\d+/g).map((n) => Number(n));
+    return new Robot(new Point3D(x, y, z), r);
+  })
+  .sort((r1, r2) => r2.radius - r1.radius);
+
+let inRangeCount = 1;
+
+for (let i = 1; i < robots.length; i++) {
+  if (manhattan3D(robots[0].location, robots[i].location) <= robots[0].radius) {
+    inRangeCount++;
+  }
+}
+
+write(YEAR, DAY, PART, inRangeCount);
