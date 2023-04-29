48f390bb9e24774d07af1e1045fb845fcd04ad93
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-04-28 20:00:57 -0400

    2021 Day 24 Complete

---
+++ 2021/24/part-2.js
@@ -0,0 +1,102 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2021, 24, 2];
+
+read(YEAR, DAY, PART);
+
+let type1 = Array(7).fill(1);
+
+function increment() {
+  let change = true;
+  let index = type1.length - 1;
+
+  while (change && index > -1) {
+    type1[index]++;
+
+    if (type1[index] === 10) {
+      type1[index] = 1;
+      index--;
+    } else {
+      change = false;
+    }
+  }
+}
+
+let type2 = Array(7).fill(null);
+
+while (true) {
+  // Digit 1
+  let z = type1[0] + 6;
+
+  // Digit 2
+  z = 26 * z + type1[1] + 6;
+
+  // Digit 3
+  z = 26 * z + type1[2] + 3;
+
+  // Digit 4
+  type2[0] = (z % 26) - 11;
+  z = Math.floor(z / 26);
+
+  // Digit 5
+  z = 26 * z + type1[3] + 9;
+
+  // Digit 6
+  type2[1] = (z % 26) - 1;
+  z = Math.floor(z / 26);
+
+  // Digit 7
+  z = 26 * z + type1[4] + 13;
+
+  // Digit 8
+  z = 26 * z + type1[5] + 6;
+
+  // Digit 9
+  type2[2] = z % 26;
+  z = Math.floor(z / 26);
+
+  // Digit 10
+  z = 26 * z + type1[6] + 10;
+
+  // Digit 11
+  type2[3] = (z % 26) - 5;
+  z = Math.floor(z / 26);
+
+  // Digit 12
+  type2[4] = (z % 26) - 16;
+  z = Math.floor(z / 26);
+
+  // Digit 13
+  type2[5] = (z % 26) - 7;
+  z = Math.floor(z / 26);
+
+  // Digit 14
+  type2[6] = (z % 26) - 11;
+  z = Math.floor(z / 26);
+
+  if (type2.some((d) => d <= 0 || d > 9)) {
+    increment();
+    continue;
+  }
+
+  if (z === 0) break;
+}
+
+const result = [
+  type1[0],
+  type1[1],
+  type1[2],
+  type2[0],
+  type1[3],
+  type2[1],
+  type1[4],
+  type1[5],
+  type2[2],
+  type1[6],
+  type2[3],
+  type2[4],
+  type2[5],
+  type2[6],
+];
+
+write(YEAR, DAY, PART, result.join(''));
---
+++ 2021/24/validator.js
@@ -0,0 +1,60 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2021, 24, 3];
+
+const instructions = read(YEAR, DAY, PART);
+const inputs = (62911941716111)
+  .toString()
+  .split('')
+  .map((n) => Number(n));
+
+const registers = new Map([
+  ['w', 0],
+  ['x', 0],
+  ['y', 0],
+  ['z', 0],
+]);
+
+let inputIndex = 0;
+
+function getValue(val) {
+  if (/[wxyz]/.test(val)) {
+    return registers.get(val);
+  }
+
+  return Number(val);
+}
+
+for (const instruction of instructions) {
+  const [command, arg1, arg2] = instruction.split(' ');
+
+  switch (command) {
+    case 'inp':
+      registers.set('w', inputs[inputIndex]);
+      inputIndex++;
+      break;
+
+    case 'mul':
+      registers.set(arg1, getValue(arg1) * getValue(arg2));
+      break;
+
+    case 'add':
+      registers.set(arg1, getValue(arg1) + getValue(arg2));
+      break;
+
+    case 'mod':
+      registers.set(arg1, getValue(arg1) % getValue(arg2));
+      break;
+
+    case 'eql':
+      const equal = getValue(arg1) === getValue(arg2);
+      registers.set(arg1, equal ? 1 : 0);
+      break;
+
+    case 'div':
+      registers.set(arg1, Math.floor(getValue(arg1) / getValue(arg2)));
+      break;
+  }
+}
+
+write(YEAR, DAY, registers.get('z'));
