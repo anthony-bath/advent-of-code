191cba5968e85e1e45ccc4bb34e80a82649117dd
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-05-04 17:02:15 -0400

    2019 Day 21 Complete

--- 2019/21/part-2.js
+++ 2019/21/part-2.js
@@ -1,7 +1,7 @@
 import { read, write } from '../../utilities/io.js';
 import { execute } from '../IntCode.js';
 
-const [YEAR, DAY, PART] = [2019, 21, 1];
+const [YEAR, DAY, PART] = [2019, 21, 2];
 
 const program = read(YEAR, DAY, PART, { splitBy: ',' }).map((n) => Number(n));
 
@@ -12,7 +12,21 @@ function toASCIICommand(text) {
 const state = { pointer: 0, program: [...program], relativeBase: 0 };
 
 const output = [];
-const commands = ['NOT A J', 'NOT B T', 'OR T J', 'NOT C T', 'OR T J', 'AND D J', 'WALK'];
+const commands = [
+  'NOT A J',
+  'NOT B T',
+  'OR T J',
+  'NOT C T',
+  'OR T J',
+  'AND D J',
+  'NOT I T',
+  'NOT T T',
+  'OR F T',
+  'AND E T',
+  'OR H T',
+  'AND T J',
+  'RUN',
+];
 const input = commands.map((command) => toASCIICommand(command)).flat();
 
 while (!state.halted) {
