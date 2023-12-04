fbc80bc8c59c76bec1fd920ea5b9c62e67c676ef
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-12-04 07:52:02 -0500

    2023 Day 4 Complete

---
+++ 2023/04/part-1.js
@@ -0,0 +1,15 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2023, 4, 1];
+const WINNING_NUMBER_COUNT = 10;
+
+const score = read(YEAR, DAY, PART).reduce((score, cardData) => {
+  const numbers = cardData.match(/\d+/g).slice(1);
+  const winningNumbers = numbers.slice(0, WINNING_NUMBER_COUNT);
+  const myNumbers = numbers.slice(WINNING_NUMBER_COUNT);
+  const matches = myNumbers.filter((n) => winningNumbers.includes(n));
+
+  return score + (matches.length > 0 ? Math.pow(2, matches.length - 1) : 0);
+}, 0);
+
+write(YEAR, DAY, PART, score);
---
+++ 2023/04/part-2.js
@@ -0,0 +1,30 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2023, 4, 2];
+const WINNING_NUMBER_COUNT = 10;
+
+const copiesByCardId = {};
+
+const count = read(YEAR, DAY, PART).reduce((count, card) => {
+  const numbers = card.match(/\d+/g);
+  const cardId = Number(numbers.shift());
+  const winningNumbers = numbers.slice(0, WINNING_NUMBER_COUNT);
+  const myNumbers = numbers.slice(WINNING_NUMBER_COUNT);
+  const matchedNumbers = myNumbers.filter((n) => winningNumbers.includes(n));
+
+  if (!copiesByCardId[cardId]) {
+    copiesByCardId[cardId] = 1;
+  }
+
+  for (let nextId = cardId + 1; nextId <= cardId + matchedNumbers.length; nextId++) {
+    if (!copiesByCardId[nextId]) {
+      copiesByCardId[nextId] = 1;
+    }
+
+    copiesByCardId[nextId] += copiesByCardId[cardId];
+  }
+
+  return count + copiesByCardId[cardId];
+}, 0);
+
+write(YEAR, DAY, PART, count);
