1d1d657254a0c618be6381d94225f84b406417ae
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-12-05 08:30:52 -0500

    2023 Day 5 Part 1

---
+++ 2023/05/part-1.js
@@ -0,0 +1,71 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2023, 5, 1];
+
+const lines = read(YEAR, DAY, PART);
+
+const seeds = lines[0].match(/\d+/g).map((n) => Number(n));
+
+const mapLabel = /(?<source>\w+)-to-(?<destination>\w+)/;
+const maps = {};
+let currentSource;
+let currentDestination;
+
+for (const line of lines.slice(2)) {
+  if (!line) continue;
+
+  const label = line.match(mapLabel);
+
+  if (label) {
+    const { source, destination } = label.groups;
+    currentSource = source;
+    currentDestination = destination;
+
+    maps[currentSource] = [];
+  } else {
+    const [drs, srs, len] = line.match(/\d+/g).map((n) => Number(n));
+
+    maps[currentSource].push({
+      min: srs,
+      max: srs + len - 1,
+      [currentDestination]: {
+        min: drs,
+        max: drs + len,
+      },
+    });
+  }
+}
+
+function getMappedTo(map) {
+  return Object.keys(map)
+    .filter((k) => !['min', 'max'].includes(k))
+    .pop();
+}
+
+function find(source, destination, value) {
+  const sourceMaps = maps[source];
+  const mappedTo = getMappedTo(sourceMaps[0]);
+
+  let targetMap = sourceMaps.find((m) => value >= m.min && value <= m.max);
+  let destinationValue;
+
+  if (!targetMap) {
+    destinationValue = value;
+  } else {
+    const diff = value - targetMap.min;
+    destinationValue = targetMap[mappedTo].min + diff;
+  }
+
+  if (mappedTo !== destination) {
+    return find(mappedTo, destination, destinationValue);
+  } else {
+    return destinationValue;
+  }
+}
+
+const minLocation = seeds
+  .map((seed) => find('seed', 'location', seed))
+  .sort((a, b) => a - b)
+  .shift();
+
+write(YEAR, DAY, PART, minLocation);
