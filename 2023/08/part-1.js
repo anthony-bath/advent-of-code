4e3a61e873b4727580a42af9be3312d0873d5ffe
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-12-08 07:38:23 -0500

    2023 Day 8 Complete

---
+++ 2023/08/part-1.js
@@ -0,0 +1,21 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2023, 8, 1];
+
+const lines = read(YEAR, DAY, PART);
+const directions = lines[0].split('');
+
+const expr = /(?<node>\w{3}) = \((?<L>\w{3}), (?<R>\w{3})\)/;
+const nodes = lines.slice(2).reduce((nodes, line) => {
+  const { node, L, R } = line.match(expr).groups;
+  return { ...nodes, [node]: { L, R } };
+}, {});
+
+let step = 0;
+let node = 'AAA';
+
+while (node !== 'ZZZ') {
+  node = nodes[node][directions[step++ % directions.length]];
+}
+
+write(YEAR, DAY, PART, step);
---
+++ 2023/08/part-2.js
@@ -0,0 +1,28 @@
+import { read, write } from '../../utilities/io.js';
+import { lcm } from '../../utilities/math.js';
+
+const [YEAR, DAY, PART] = [2023, 8, 2];
+
+const lines = read(YEAR, DAY, PART);
+const directions = lines[0].split('');
+
+const expr = /(?<node>\w{3}) = \((?<L>\w{3}), (?<R>\w{3})\)/;
+const nodes = lines.slice(2).reduce((nodes, line) => {
+  const { node, L, R } = line.match(expr).groups;
+  return { ...nodes, [node]: { L, R } };
+}, {});
+
+let currentNodes = Object.keys(nodes).filter((node) => node.endsWith('A'));
+
+const steps = currentNodes.map((node) => {
+  let current = node;
+  let step = 0;
+
+  while (!current.endsWith('Z')) {
+    current = nodes[current][directions[step++ % directions.length]];
+  }
+
+  return step;
+});
+
+write(YEAR, DAY, PART, lcm(...steps));
