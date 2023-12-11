76fcb563df204335c243ca3384678918e04c1fbb
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-12-11 06:57:42 -0500

    2023 Day 11 Complete

---
+++ 2023/11/common.js
@@ -0,0 +1,47 @@
+import { read } from '../../utilities/io.js';
+
+export function loadData(year, day, part) {
+  const emptyRows = [];
+  const emptyCols = [];
+  const galaxies = [];
+
+  const grid = read(year, day, part).map((line, y) => {
+    let hasGalaxies = false;
+    const row = line.split('');
+
+    for (let x = 0; x < line.length; x++) {
+      if (row[x] === '#') {
+        galaxies.push({ id: galaxies.length + 1, x, y });
+        hasGalaxies = true;
+      }
+    }
+
+    if (!hasGalaxies) {
+      emptyRows.push(y);
+    }
+
+    return line;
+  });
+
+  const rowIndices = [...Array(grid[0].length).keys()];
+
+  for (let x = 0; x < grid[0].length; x++) {
+    if (rowIndices.every((y) => grid[y][x] === '.')) {
+      emptyCols.push(x);
+    }
+  }
+
+  return { galaxies, emptyCols, emptyRows };
+}
+
+export function getBlankSpaceBetween(g1, g2, cols, rows, factor) {
+  const xMin = Math.min(g1.x, g2.x);
+  const yMin = Math.min(g1.y, g2.y);
+  const xMax = Math.max(g1.x, g2.x);
+  const yMax = Math.max(g1.y, g2.y);
+
+  const colsBetween = cols.filter((col) => col > xMin && col < xMax).length;
+  const rowsBetween = rows.filter((row) => row > yMin && row < yMax).length;
+
+  return (colsBetween + rowsBetween) * (factor - 1);
+}
---
+++ 2023/11/part-2.js
@@ -0,0 +1,18 @@
+import { pairs } from '../../utilities/array.js';
+import { write } from '../../utilities/io.js';
+import { manhattan } from '../../utilities/math.js';
+import { getBlankSpaceBetween, loadData } from './common.js';
+
+const [YEAR, DAY, PART] = [2023, 11, 2];
+const { galaxies, emptyCols, emptyRows } = loadData(YEAR, DAY, PART);
+
+write(
+  YEAR,
+  DAY,
+  PART,
+  pairs(galaxies).reduce(
+    (total, [g1, g2]) =>
+      total + manhattan(g1, g2) + getBlankSpaceBetween(g1, g2, emptyCols, emptyRows, 1e6),
+    0
+  )
+);
