54a380c9df0b2e550a8a851a8fbe75d67265a5d0
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-12-12 13:04:02 -0500

    2023 Day 12 Part 1

---
+++ 2023/12/part-1.js
@@ -0,0 +1,52 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2023, 12, 1];
+
+function generateCombinations(str) {
+  const combinations = [];
+
+  function generate(current, index) {
+    if (index === str.length) {
+      combinations.push(current);
+      return;
+    }
+
+    if (str[index] === '?') {
+      generate(current + '.', index + 1);
+      generate(current + '#', index + 1);
+    } else {
+      generate(current + str[index], index + 1);
+    }
+  }
+
+  generate('', 0);
+  return combinations;
+}
+
+const count = read(YEAR, DAY, PART).reduce((count, line) => {
+  const [arrangement, springList] = line.split(' ');
+  const springCounts = springList.match(/\d+/g);
+
+  const springPattern = springCounts.reduce((expr, count, i) => {
+    if (i === springCounts.length - 1) {
+      return `${expr}#{${count}}`;
+    }
+
+    return `${expr}#{${count}}\\.{1,}`;
+  }, '');
+
+  const pattern = `^\\.{0,}${springPattern}\\.{0,}$`;
+  const expr = new RegExp(pattern);
+
+  let currentCount = 0;
+
+  for (const combo of generateCombinations(arrangement)) {
+    if (expr.test(combo)) {
+      currentCount++;
+    }
+  }
+
+  return count + currentCount;
+}, 0);
+
+write(YEAR, DAY, PART, count);
