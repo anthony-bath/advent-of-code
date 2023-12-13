9fe7ff7a056830bfc078b5a1ce7c9b0b8e9f41a0
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-12-12 20:53:06 -0500

    2023 Day 12 Complete

---
+++ 2023/12/part-2.js
@@ -0,0 +1,51 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2023, 12, 1];
+
+function count(arrangement, groups) {
+  if (!arrangement) {
+    return !groups.length ? 1 : 0;
+  }
+
+  if (!groups.length) {
+    return arrangement.includes('#') ? 0 : 1;
+  }
+
+  const key = `${arrangement}-${groups}`;
+
+  if (key in cache) {
+    return cache[key];
+  }
+
+  let result = 0;
+
+  if (['?', '.'].includes(arrangement[0])) {
+    result += count(arrangement.slice(1), groups);
+  }
+
+  if (['?', '#'].includes(arrangement[0])) {
+    if (
+      groups[0] <= arrangement.length &&
+      !arrangement.slice(0, groups[0]).includes('.') &&
+      (groups[0] === arrangement.length || arrangement[groups[0]] !== '#')
+    ) {
+      result += count(arrangement.slice(groups[0] + 1), groups.slice(1));
+    }
+  }
+
+  cache[key] = result;
+  return result;
+}
+
+const cache = {};
+
+const total = read(YEAR, DAY, PART).reduce((total, line) => {
+  const [arrangement, springList] = line.split(' ');
+  const groups = springList.match(/\d+/g).map((n) => Number(n));
+  const unfoldedGroups = Array(5).fill(groups).flat();
+  const unfoledArrangement = Array(5).fill(arrangement).join('?');
+
+  return total + count(unfoledArrangement, unfoldedGroups);
+}, 0);
+
+write(YEAR, DAY, PART, total);
