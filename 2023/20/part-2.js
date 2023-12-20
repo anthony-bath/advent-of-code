a32a6f91ef45da1e6f614803fd63c8fb7fa88b25
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-12-20 11:11:35 -0500

    2023 Day 20 Complete

---
+++ 2023/20/part-2.js
@@ -0,0 +1,64 @@
+import { write } from '../../utilities/io.js';
+import { PULSE, Network } from './common.js';
+import { lcm } from '../../utilities/math.js';
+
+const [YEAR, DAY, PART] = [2023, 20, 2];
+const network = new Network(YEAR, DAY, PART);
+const modules = [...network.flipflops.values(), ...network.conjunctions.values()];
+
+let rxInput;
+
+for (const module of modules) {
+  if (module.outputs.includes('rx')) {
+    rxInput = module.name;
+    break;
+  }
+}
+
+let rxInputInputs = [];
+
+for (const module of modules) {
+  if (module.outputs.includes(rxInput)) {
+    rxInputInputs.push(module.name);
+  }
+}
+
+let cycles = [];
+let presses = 0;
+
+while (rxInputInputs.length) {
+  presses += 1;
+
+  const queue = [{ type: PULSE.LOW, target: 'broadcaster', origin: 'button' }];
+
+  while (queue.length) {
+    const { type, target, origin } = queue.shift();
+
+    let pulses = [];
+
+    switch (true) {
+      case target === 'broadcaster':
+        pulses = network.broadcaster.onReceivePulse(type);
+        break;
+
+      case network.flipflops.has(target):
+        pulses = network.flipflops.get(target).onReceivePulse(type);
+        break;
+
+      case network.conjunctions.has(target):
+        pulses = network.conjunctions.get(target).onReceivePulse(type, origin);
+        break;
+    }
+
+    for (const pulse of pulses) {
+      if (rxInputInputs.includes(pulse.target) && pulse.type === PULSE.LOW) {
+        cycles.push(presses);
+        rxInputInputs = rxInputInputs.filter((input) => input !== pulse.target);
+      }
+
+      queue.push(pulse);
+    }
+  }
+}
+
+write(YEAR, DAY, PART, lcm(...cycles));
