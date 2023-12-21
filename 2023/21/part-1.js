16e4cbcbdfa14154ffdef682a9c2bd6acdda829e
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-12-21 18:52:54 -0500

    2023 Day 21 Complete

---
+++ 2023/21/part-1.js
@@ -0,0 +1,65 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2023, 21, 1];
+
+const grid = read(YEAR, DAY, PART).map((line) => line.split(''));
+
+let start;
+
+for (let y = 0; y < grid.length; y++) {
+  for (let x = 0; x < grid[y].length; x++) {
+    if (grid[y][x] === 'S') {
+      start = { x, y };
+      break;
+    }
+  }
+}
+
+const deltas = [
+  [-1, 0],
+  [0, -1],
+  [1, 0],
+  [0, 1],
+];
+
+const distance = Array(grid.length)
+  .fill()
+  .map(() => Array(grid[0].length).fill(Infinity));
+
+distance[start.y][start.x] = 0;
+
+const queue = [[start.x, start.y]];
+
+while (queue.length) {
+  const [cx, cy] = queue.shift();
+
+  for (const [dx, dy] of deltas) {
+    const nx = cx + dx;
+    const ny = cy + dy;
+
+    if (nx < 0 || nx >= grid[0].length || ny < 0 || ny >= grid.length) {
+      continue;
+    }
+
+    if (grid[ny][nx] === '#') {
+      continue;
+    }
+
+    if (distance[cy][cx] + 1 < distance[ny][nx]) {
+      distance[ny][nx] = distance[cy][cx] + 1;
+      queue.push([nx, ny]);
+    }
+  }
+}
+
+let points = 0;
+
+for (let y = 0; y < grid.length; y++) {
+  for (let x = 0; x < grid[y].length; x++) {
+    if (distance[y][x] <= 64 && distance[y][x] % 2 === 0) {
+      points++;
+    }
+  }
+}
+
+write(YEAR, DAY, PART, points);
---
+++ 2023/21/part-2.js
@@ -0,0 +1,76 @@
+import { read, write } from '../../utilities/io.js';
+
+const [YEAR, DAY, PART] = [2023, 21, 2];
+
+const grid = read(YEAR, DAY, PART).map((line) => line.split(''));
+
+let start;
+
+for (let y = 0; y < grid.length; y++) {
+  for (let x = 0; x < grid[y].length; x++) {
+    if (grid[y][x] === 'S') {
+      start = { x, y };
+      break;
+    }
+  }
+}
+
+const deltas = [
+  [-1, 0],
+  [0, -1],
+  [1, 0],
+  [0, 1],
+];
+
+const distances = Array(grid.length)
+  .fill()
+  .map(() => Array(grid[0].length).fill(Infinity));
+
+distances[start.y][start.x] = 0;
+
+const queue = [[start.x, start.y]];
+
+while (queue.length) {
+  const [cx, cy] = queue.shift();
+
+  for (const [dx, dy] of deltas) {
+    const nx = cx + dx;
+    const ny = cy + dy;
+
+    if (nx < 0 || nx >= grid[0].length || ny < 0 || ny >= grid.length) {
+      continue;
+    }
+
+    if (grid[ny][nx] === '#') {
+      continue;
+    }
+
+    if (distances[cy][cx] + 1 < distances[ny][nx]) {
+      distances[ny][nx] = distances[cy][cx] + 1;
+      queue.push([nx, ny]);
+    }
+  }
+}
+
+const evenDistances = distances.flat().filter((distance) => distance % 2 === 0).length;
+const oddDistances = distances.flat().filter((distance) => distance % 2 === 1).length;
+
+const oddCornerDistances = distances
+  .flat()
+  .filter((distance) => distance > 65 && distance % 2 === 1).length;
+
+const evenCornerDistances = distances
+  .flat()
+  .filter((distance) => distance > 65 && distance % 2 === 0).length;
+
+const STEPS = 26501365;
+const n = Math.floor(STEPS / 131);
+
+const total =
+  Math.pow(n + 1, 2) * oddDistances +
+  Math.pow(n, 2) * evenDistances -
+  (n + 1) * oddCornerDistances +
+  n * evenCornerDistances -
+  n;
+
+write(YEAR, DAY, PART, total);
