dc1dba9997b62d0af705a6d1fa6bde0c58c2015d
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2023-12-24 09:17:48 -0500

    2023 Day 24 Complete

--- 2023/24/part-1.js
+++ 2023/24/part-1.js
@@ -3,10 +3,7 @@ import { read, write } from '../../utilities/io.js';
 
 const [YEAR, DAY, PART] = [2023, 24, 1];
 
-const hailstones = read(YEAR, DAY, PART).map((line) => {
-  return line.match(/-?\d+/g).map(Number);
-});
-
+const hailstones = read(YEAR, DAY, PART).map((line) => line.match(/-?\d+/g).map(Number));
 const combinations = pairs(hailstones);
 
 const MIN_POS = 200000000000000;
@@ -15,22 +12,16 @@ const MAX_POS = 400000000000000;
 let count = 0;
 
 for (const [h1, h2] of combinations) {
-  const [x1, y1, z1, vx1, vy1, vz1] = h1;
-  const [x2, y2, z2, vx2, vy2, vz2] = h2;
-
-  const h1p2 = [x1 + vx1, y1 + vy1, z1 + vz1];
-  const h2p2 = [x2 + vx2, y2 + vy2, z2 + vz2];
-
-  const [x1p2, y1p2, z1p2] = h1p2;
-  const [x2p2, y2p2, z2p2] = h2p2;
+  const [x1, y1, , vx1, vy1] = h1;
+  const [x2, y2, , vx2, vy2] = h2;
 
   // y = mx + b
-  const h1m = (y1p2 - y1) / (x1p2 - x1);
+  const h1m = vy1 / vx1;
   const h1b = y1 - h1m * x1;
-  const h2m = (y2p2 - y2) / (x2p2 - x2);
+  const h2m = vy2 / vx2;
   const h2b = y2 - h2m * x2;
 
-  // Find intersection of two lines
+  // Find intersection of the two lines
   const x = (h2b - h1b) / (h1m - h2m);
   const y = h1m * x + h1b;
 
@@ -39,8 +30,6 @@ for (const [h1, h2] of combinations) {
     // Time  to reach intersection
     const h1t = (x - x1) / vx1;
     const h2t = (x - x2) / vx2;
-
-    console.log(h1t, h2t);
 
     if (h1t >= 0 && h2t >= 0) {
       count++;
--- 2023/24/part-2.js
+++ 2023/24/part-2.js
@@ -3,44 +3,36 @@ import { init } from 'z3-solver';
 
 const [YEAR, DAY, PART] = [2023, 24, 2];
 
-const hailstones = read(YEAR, DAY, PART).map((line) => {
-  return line.match(/-?\d+/g).map(Number);
-});
+const hailstones = read(YEAR, DAY, PART).map((line) => line.match(/-?\d+/g).map(Number));
 
-const {
-  Z3, // Low-level C-like API
-  Context, // High-level Z3Py-like API
-} = await init();
+async function solve() {
+  const { Context } = await init();
+  const { Solver, Int } = new Context('main');
+  const solver = new Solver();
 
-const { Solver, Int } = new Context('main');
+  let x = Int.const('x');
+  let y = Int.const('y');
+  let z = Int.const('z');
+  let vx = Int.const('vx');
+  let vy = Int.const('vy');
+  let vz = Int.const('vz');
 
-const solver = new Solver();
-let x = Int.const('x');
-let y = Int.const('y');
-let z = Int.const('z');
-let vx = Int.const('vx');
-let vy = Int.const('vy');
-let vz = Int.const('vz');
+  const t = [0, 1, 2].map((i) => Int.const(`t${i}`));
 
-const t = [0, 1, 2].map((i) => Int.const(`f't${i}`));
+  hailstones.slice(0, 3).forEach(([hx, hy, hz, hvx, hvy, hvz], i) => {
+    solver.add(t[i].mul(hvx).add(hx).sub(x).sub(t[i].mul(vx)).eq(0));
+    solver.add(t[i].mul(hvy).add(hy).sub(y).sub(t[i].mul(vy)).eq(0));
+    solver.add(t[i].mul(hvz).add(hz).sub(z).sub(t[i].mul(vz)).eq(0));
+  });
 
-const [hs1x, hs1y, hs1z, hs1vx, hs1vy, hs1vz] = hailstones[0];
-const [hs2x, hs2y, hs2z, hs2vx, hs2vy, hs2vz] = hailstones[1];
-const [hs3x, hs3y, hs3z, hs3vx, hs3vy, hs3vz] = hailstones[2];
+  const result = await solver.check();
 
-solver.add(x + t[0] * vx - hs1x - t[0] * hs1vx == 0);
-solver.add(y + t[0] * vy - hs1y - t[0] * hs1vy == 0);
-solver.add(z + t[0] * vz - hs1z - t[0] * hs1vz == 0);
+  if (result !== 'sat') {
+    throw new Error('Unsatisfiable');
+  }
 
-solver.add(x + t[1] * vx - hs2x - t[1] * hs2vx == 0);
-solver.add(y + t[1] * vy - hs2y - t[1] * hs2vy == 0);
-solver.add(z + t[1] * vz - hs2z - t[1] * hs2vz == 0);
+  return solver.model().eval(x.add(y).add(z)).value();
+}
 
-solver.add(x + t[2] * vx - hs3x - t[2] * hs3vx == 0);
-solver.add(y + t[2] * vy - hs3y - t[2] * hs3vy == 0);
-solver.add(z + t[2] * vz - hs3z - t[2] * hs3vz == 0);
-
-const result = await solver.check();
-console.log(result);
-
-write(YEAR, DAY, PART, '');
+write(YEAR, DAY, PART, await solve());
+process.exit(); //seems to be some issue with z3-solver keeping the process alive
