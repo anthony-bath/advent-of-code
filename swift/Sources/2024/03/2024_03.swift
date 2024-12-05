a8f66bae293b1b03a2d3d8be7830fa3803ab1a4b
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2024-12-05 12:43:59 -0500

    refactor: simplify day 3 approach

--- swift/Sources/2024/03/2024_03.swift
+++ swift/Sources/2024/03/2024_03.swift
@@ -3,54 +3,29 @@ import Algorithms
 extension Year2024 {
   struct Day03: AdventDay {
     var data: String
-    var muls: [Regex<Regex<(Substring, X: Substring, Y: Substring)>.RegexOutput>.Match]
-
-    init(data: String, lines _: [String]) {
-      self.data = data
-
-      let mulExpr = #/mul\((?<X>\d{1,3}),(?<Y>\d{1,3})\)/#
-      muls = data.matches(of: mulExpr)
-    }
+    var lines: [String]
 
     func part1() -> Any {
-      muls.reduce(0) { $0 + Int($1.output.X)! * Int($1.output.Y)! }
+      let expr = #/mul\((?<X>\d{1,3}),(?<Y>\d{1,3})\)/#
+      let muls = data.matches(of: expr)
+
+      return muls.reduce(0) { $0 + Int($1.output.X)! * Int($1.output.Y)! }
     }
 
     func part2() -> Any {
-      enum OpType {
-        case enableMul
-        case disableMul
-        case mul(Int, Int)
-      }
-
-      let dos = data.matches(of: #/do\(\)/#)
-      let donts = data.matches(of: #/don't\(\)/#)
-
-      var ops = [(type: OpType, index: String.Index)]()
-
-      for doOp in dos {
-        ops.append((.enableMul, doOp.range.lowerBound))
-      }
-
-      for dontOp in donts {
-        ops.append((.disableMul, dontOp.range.lowerBound))
-      }
-
-      for mul in muls {
-        ops.append((.mul(Int(mul.output.X)!, Int(mul.output.Y)!), mul.range.lowerBound))
-      }
-
-      ops.sort { $0.index < $1.index }
+      let expr = #/do\(\)|don't\(\)|mul\((?<X>\d{1,3}),(?<Y>\d{1,3})\)/#
+      let ops = data.matches(of: expr)
 
       var mulEnabled = true
       var total = 0
 
       for op in ops {
-        switch op.type {
-        case .enableMul: mulEnabled = true
-        case .disableMul: mulEnabled = false
-        case let .mul(x, y):
-          if mulEnabled { total += x * y }
+        if op.output.0 == "do()" {
+          mulEnabled = true
+        } else if op.output.0 == "don't()" {
+          mulEnabled = false
+        } else if mulEnabled {
+          total += Int(op.output.X!)! * Int(op.output.Y!)!
         }
       }
 
