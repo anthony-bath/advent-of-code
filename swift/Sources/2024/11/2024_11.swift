a6d8b58a0e5c3dcbbb85d3bfff4aa07129411dee
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2024-12-15 10:10:29 -0500

    feat: 2024 day 11 complete

--- swift/Sources/2024/11/2024_11.swift
+++ swift/Sources/2024/11/2024_11.swift
@@ -2,12 +2,14 @@ import Algorithms
 
 extension Year2024 {
   struct Day11: AdventDay {
-    var data: String
-    var lines: [String]
+    var stones: [Int]
+
+    init(data: String, lines _: [String]) {
+      stones = data.split(separator: " ").map { Int($0)! }
+    }
 
     func part1() -> Any {
-      var stones = data.split(separator: " ").map { Int($0)! }
-      var seen = Set<Int>()
+      var stones = self.stones
 
       for _ in 0 ..< 25 {
         let count = stones.count
@@ -28,9 +30,6 @@ extension Year2024 {
 
             stones[i] = left
             stones.append(right)
-
-            seen.insert(left)
-            seen.insert(right)
           } else {
             stones[i] *= 2024
           }
@@ -40,8 +39,53 @@ extension Year2024 {
       return stones.count
     }
 
+    struct CacheItem: Hashable {
+      var stone: Int
+      var depth: Int
+    }
+
     func part2() -> Any {
-      0
+      var cache = [CacheItem: Int]()
+
+      func blink(stone: Int) -> (Int, Int?) {
+        if stone == 0 {
+          return (1, nil)
+        }
+
+        let str = String(stone)
+
+        if str.count % 2 == 0 {
+          let left = Int(str.prefix(str.count / 2))!
+          let right = Int(str.suffix(str.count / 2))!
+
+          return (left, right)
+        }
+
+        return (stone * 2024, nil)
+      }
+
+      func dfs(stone: Int, depth: Int) -> Int {
+        if let cached = cache[CacheItem(stone: stone, depth: depth)] {
+          return cached
+        }
+
+        let (left, right) = blink(stone: stone)
+
+        if depth == 75 {
+          return right != nil ? 2 : 1
+        }
+
+        var result = dfs(stone: left, depth: depth + 1)
+
+        if right != nil {
+          result += dfs(stone: right!, depth: depth + 1)
+        }
+
+        cache[CacheItem(stone: stone, depth: depth)] = result
+        return result
+      }
+
+      return stones.reduce(0) { $0 + dfs(stone: $1, depth: 1) }
     }
   }
 }
--- swift/Tests/2024Tests/2024.11.test.swift
+++ swift/Tests/2024Tests/2024.11.test.swift
@@ -9,6 +9,6 @@ final class Year2024Day11Tests: XCTestCase {
   }
 
   func testPart2() {
-    XCTAssertEqual(day.part2() as? Int, 0)
+    XCTAssertEqual(day.part2() as? Int, 221_280_540_398_419)
   }
 }
