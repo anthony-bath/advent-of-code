7cacdcac8563eda4e62226fdb0e7432ef71546bd
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2025-12-01 20:56:49 -0500

    feat: 2025 day 1 complete

--- swift/Sources/2025/01/2025.01.swift
+++ swift/Sources/2025/01/2025.01.swift
@@ -1,4 +1,5 @@
 import Algorithms
+import Foundation
 
 extension Year2025 {
   struct Day01: AdventDay {
@@ -6,11 +7,60 @@ extension Year2025 {
     var lines: [String]
 
     func part1() -> Any {
-      0
+      var position = 50
+      var password = 0
+
+      lines.forEach {
+        let dir = String($0.prefix(1))
+        let distance = Int(String($0.dropFirst(1)))! % 100
+        let turns = dir == "L" ? 100 - distance : distance
+
+        position = (position + turns) % 100
+
+        if position == 0 {
+          password += 1
+        }
+      }
+
+      return password
     }
 
     func part2() -> Any {
-      0
+      var position = 50
+      var password = 0
+
+      lines.forEach {
+        let dir = String($0.prefix(1))
+        let distance = Int(String($0.dropFirst(1)))!
+
+        if distance >= 100 {
+          // Full Rotations
+          password += (distance / 100)
+        }
+
+        let remaining = distance % 100
+
+        if dir == "L" {
+          let initial = position
+          let next = (position - remaining + 100) % 100
+
+          if position - remaining < 0 && initial != 0 {
+            password += 1
+          } else if next == 0 && position != 0 && remaining > 0 {
+            password += 1
+          }
+
+          position = next
+        } else {
+          if position + remaining > 99 {
+            password += 1
+          }
+
+          position = (position + remaining) % 100
+        }
+      }
+
+      return password
     }
   }
 }
--- swift/Tests/2025Tests/2025.01.test.swift
+++ swift/Tests/2025Tests/2025.01.test.swift
@@ -5,10 +5,10 @@ struct Year2025Day01Tests {
   let day = Year2025.Day01(challengeYear: 2025)
 
   @Test("Day 1, Part 1") func part1() {
-    #expect(day.part1() as? Int == 0)
+    #expect(day.part1() as? Int == 1081)
   }
 
   @Test("Day 1, Part 2") func part2() {
-    #expect(day.part2() as? Int == 0)
+    #expect(day.part2() as? Int == 6689)
   }
 }
